a50a82b0fc3ae9b66d714d653f42e65907723ce6
Axel <axel.schnurer@gmail.com>
2024-11-27 23:24:58 +0100

    Add conditional rendering to prevent layout issues when information is missing

--- src/app/roadmap/[roadmapId]/page.tsx
+++ src/app/roadmap/[roadmapId]/page.tsx
@@ -26,10 +26,12 @@ export default async function Page({ params }: { params: { roadmapId: string } }
   }
 
   return <>
-    <p>{roadmap.description}</p>
-
+    {roadmap.description ? (
+      <p>{roadmap.description}</p>
+    ): null}
+    
     <div
-      className="grid gap-100 margin-block-100 padding-block-100 align-items-flex-end"
+      className="grid gap-100 margin-bottom-100 padding-block-100 align-items-flex-end"
       style={{
         borderBottom: '2px solid var(--gray-90)',
         gridTemplateColumns: 'repeat(auto-fit, 300px)'
@@ -38,7 +40,9 @@ export default async function Page({ params }: { params: { roadmapId: string } }
       {roadmap.goals.map((goal, key) =>
         goal.isFeatured ?
           <div key={key}>
-            <h3 style={{ textAlign: 'center' }}>{goal.name}</h3>
+            {goal.name ? (
+              <h3 style={{ textAlign: 'center' }}>{goal.name}</h3>
+            ): null }
             <a href={`/goal/${goal.id}`}>
               <ThumbnailGraph goal={goal} />
             </a>
