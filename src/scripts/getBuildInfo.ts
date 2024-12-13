c1d4db86e2eeed36e95c951a88744d431048f94e
Leon-Loov <leon.loov@sustainable-action.org>
2024-12-13 13:58:23 +0100

    Update scripts to fail more graciously

--- src/scripts/getBuildInfo.ts
+++ src/scripts/getBuildInfo.ts
@@ -1,34 +1,37 @@
 import fs from 'fs';
 import { execSync } from "child_process";
 
+/**
+ * This script generates a json file containing the short and long commit hashes of the current commit.
+ * The file is used to display the commit hash of the current build in the information page and link to the commit on GitHub.
+ * The output is saved in src/lib/commitHash.json
+ */
 async function getBuildInfo() {
-  const info: { [key: string]: string } = {}
+  const info: { shortHash?: string, longHash?: string } = {}
 
-  // Get short commit hash
-  const shortHash = execSync('git rev-parse --short HEAD')?.toString().trim();
+  // Try getting commit hashes
+  try {
+    const shortHash = execSync('git rev-parse --short HEAD')?.toString().trim();
+    const longHash = execSync('git rev-parse HEAD')?.toString().trim();
 
-  console.log("The current commit hash is: " + shortHash)
-  info.shortHash = shortHash;
-
-
-  // Get remote URL
-  let remoteUrl = execSync('git config --get remote.origin.url')?.toString().trim();
-  // Remove .git from the end of the URL so we can append the path to the commit
-  if (remoteUrl.endsWith('.git')) {
-    remoteUrl = remoteUrl.slice(0, -4);
+    console.log(`The current commit hash is: ${shortHash || "no short hash"} (${longHash || "no long hash"})`)
+    info.shortHash = shortHash;
+    info.longHash = longHash;
+  } catch {
+    console.log("Failed to get commit hashes")
   }
-  if (!remoteUrl.endsWith('/')) {
-    remoteUrl += '/';
+
+  if (info.shortHash || info.longHash) {
+    // Try to write info to file
+    try {
+      fs.writeFileSync('src/lib/commitHash.json', JSON.stringify(info));
+      console.log("Commit hash info updated");
+    } catch {
+      console.log("Failed to write commit hashes to file");
+    }
+  } else {
+    console.log("No commit hashes found; file untouched");
   }
-  console.log("The current remote URL is: " + remoteUrl)
-  info.remoteURL = remoteUrl;
-
-
-  // Write info to file
-  fs.writeFile('src/lib/version.json', JSON.stringify(info), (err) => {
-    if (err) throw err;
-    console.log("Git-related build info has been saved")
-  });
 }
 
 getBuildInfo();
