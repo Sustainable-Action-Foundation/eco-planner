797b60d868900f6a12edfb0236022fcb2050d03a
Axel <axel.schnurer@gmail.com>
2025-01-30 21:25:49 +0100

    Update open graph tags and remove unused images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,38 @@
 import '@/styles/global.css'
 import Sidebar from '@/components/generic/header/sidebar'
 import styles from './page.module.css' with { type: "css" }
+import { headers } from "next/headers";
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode,
 }) {
+
   return (
     <html lang="sv">
       <head>
+        {/* TODO: Lots of this should be dynamic probably */}
         <title>Eco - Planner</title>
         <link rel="icon" type="image/x-icon" href="/icons/leaf.svg" />
-
-
         <meta name="description" content="Ett verktyg som syftar till att bidra till Sveriges klimatomställning. 
         I verktyget kan nationella scenarier, även kallade kvantitativa färdplaner, brytas ner till regional och lokal nivå och en handlingsplan kan skapas. 
         Handlingsplanen byggs upp av åtgärder vilka relaterar till en specifik målbana och målbanorna utgör tillsammans hela färdplanen. 
         Användare kan inspireras av varandras åtgärder, på så sätt skapas en gemensam åtgärdsdatabas för Sverige. På lokal nivå kan också olika aktörer samarbeta kring åtgärder. "/>
 
         {/* Open graph Meta Tags */}
-        <meta name="og:site_name" content="Eco - Planner" />
-        <meta property="og:url" content="https://stuns.molnet.xyz/" />
-        <meta property="og:type" content="website" />
+        {/* Required tags */}
         <meta property="og:title" content="Eco - Planner" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={headers().get('referer') || ''} />
+        <meta property="og:image" content="https://stuns.molnet.xyz/images/solarpanels.jpg" />
+        
+        {/* Optional tags */}
+        <meta name="og:site_name" content="Eco - Planner" />
         <meta property="og:description" content="Ett verktyg som syftar till att bidra till Sveriges klimatomställning. 
         I verktyget kan nationella scenarier, även kallade kvantitativa färdplaner, brytas ner till regional och lokal nivå och en handlingsplan kan skapas. 
         Handlingsplanen byggs upp av åtgärder vilka relaterar till en specifik målbana och målbanorna utgör tillsammans hela färdplanen. 
         Användare kan inspireras av varandras åtgärder, på så sätt skapas en gemensam åtgärdsdatabas för Sverige. På lokal nivå kan också olika aktörer samarbeta kring åtgärder. "/>
-        <meta property="og:image" content="https://stuns.molnet.xyz/images/roadmap.jpg" />
 
       </head>
       <body>
