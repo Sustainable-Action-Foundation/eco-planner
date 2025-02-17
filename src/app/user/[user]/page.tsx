31d9413d272f59ae5e933e435fc4694d34183324
Axel <axel.schnurer@gmail.com>
2025-02-17 20:45:32 +0100

    Updated userpage appearance

--- src/app/user/[user]/page.tsx
+++ src/app/user/[user]/page.tsx
@@ -7,7 +7,6 @@ import accessChecker from '@/lib/accessChecker';
 import { getSession } from '@/lib/session';
 import { AccessLevel } from '@/types';
 import { cookies } from 'next/headers';
-import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { user: string } }) {
@@ -58,68 +57,91 @@ export default async function Page({ params }: { params: { user: string } }) {
             <h2>Hantera min data</h2>
             <GraphCookie />
           </section>
-
-          <section className='margin-block-300'>
-          <h2>Färdplansserier jag kan redigera</h2>
-          <ul>
-            {editableMetaRoadmaps.map((editableMetaRoadmap, index) => 
-              <li key={index} className='margin-block-25'>
-                <div className='flex justify-content-space-between align-items-center'>
-                  <div>
-                    <a href={`/metaRoadmap/${editableMetaRoadmap.id}`}>{editableMetaRoadmap.name}</a> <br/>
-                    <small>Antal versioner i denna serie: {editableMetaRoadmap.roadmapVersions.length}</small>
-                  </div>
-                  <TableMenu object={editableMetaRoadmap} />
-                </div>
-              </li>
-            )}
-          </ul>
-          </section>
-
-          <section className='margin-block-300'>
-          <h2>Färdplaner jag kan redigera</h2>
-          <ul>
-            {editableRoadmaps.map((editableRoadmap, index) => 
-              <li key={index} className='margin-block-25'>
-                <div className='flex justify-content-space-between align-items-center'>
-                  <div>
-                    <a href={`/roadmap/${editableRoadmap.id}`}>{editableRoadmap.metaRoadmap.name}</a> <br/> {/* TODO: Check if naming of roadmap is always inherited */}
-                    <span>Antal målbanor: {editableRoadmap._count.goals}</span>
-                  </div>
-                  <TableMenu object={editableRoadmap} />
-                </div>
-              </li>
-            )}
-          </ul>
-          </section>
         </>
       : null}
 
       <section className='margin-block-300'>
-        <h2>Mina färdplansserier</h2>
+        <h2 className='margin-bottom-100 padding-bottom-50' style={{borderBottom: '1px solid var(--gray)'}}>
+          {session.user?.username === username ?
+            'Mina objekt'
+          :
+            `@${userdata.username}'s objekt`
+          }
+        </h2>
+        <menu className='margin-0 padding-0 flex gap-200 flex-wrap-wrap margin-bottom-100'>
+          <fieldset className='flex gap-50'>
+            <legend className='font-weight-500 padding-bottom-75'>Objekt</legend>
+            <label className='flex gap-25 align-items-center'>
+              <input type='checkbox' />
+              Färdplan
+            </label>
+            <label className='flex gap-25 align-items-center'>
+              <input type='checkbox' />
+              Färdplansserie
+            </label>
+          </fieldset>
+
+          {session.user?.username === username ?
+            <fieldset className='flex gap-50'>
+              <legend className='font-weight-500 padding-bottom-75'>Behörighet</legend>
+              <label className='flex gap-25 align-items-center'>
+                <input type='checkbox' />
+                Ägandeskap
+              </label>
+              <label className='flex gap-25 align-items-center'>
+                <input type='checkbox' />
+                Redigeringsbehörighet
+              </label>
+            </fieldset>
+          : null }
+        </menu> 
+
         <ul>
+          {/* If on users own page, show roadmapsseries and roadmaps with edit access*/}
+          {session.user?.username === username ?
+            <>
+              {editableMetaRoadmaps.map((editableMetaRoadmap, index) => 
+                <li key={index} className='margin-block-25'>
+                  <div className='flex justify-content-space-between align-items-center'>
+                    <div>
+                      <a href={`/metaRoadmap/${editableMetaRoadmap.id}`}>{editableMetaRoadmap.name}</a> • <span>Färdplansserie</span> <br/>
+                      <small>Antal versioner i denna serie: {editableMetaRoadmap.roadmapVersions.length}</small>
+                    </div>
+                    <TableMenu object={editableMetaRoadmap} />
+                  </div>
+                </li>
+              )}
+              {editableRoadmaps.map((editableRoadmap, index) => 
+                <li key={index} className='margin-block-25'>
+                  <div className='flex justify-content-space-between align-items-center'>
+                    <div>
+                      <a href={`/roadmap/${editableRoadmap.id}`}>{editableRoadmap.metaRoadmap.name}</a> • <span>Färdplan</span> <br/> {/* TODO: Check if naming of roadmap is always inherited */}
+                      <span>Antal målbanor: {editableRoadmap._count.goals}</span>
+                    </div>
+                    <TableMenu object={editableRoadmap} />
+                  </div>
+                </li>
+              )}
+            </>
+          : null}
+
+          {/* Otherwise default to show roadmapsseries and roadmaps with ownership */}
           {userdata.authoredMetaRoadmaps.map((authoredMetaRoadmap, index) => 
             <li key={index} className='margin-block-25'>
               <div className='flex justify-content-space-between align-items-center'>
                 <div>
-                  <a href={`/metaRoadmap/${authoredMetaRoadmap.id}`}>{authoredMetaRoadmap.name}</a> <br/>
+                  <a href={`/metaRoadmap/${authoredMetaRoadmap.id}`}>{authoredMetaRoadmap.name}</a> • <span>Färdplansserie</span> <br/>
                   <span>Antal versioner i denna serie: {authoredMetaRoadmap.roadmapVersions.length}</span>
                 </div>
                 <TableMenu object={authoredMetaRoadmap} />
               </div>
             </li>
           )}
-        </ul>
-      </section>
-
-      <section className='margin-block-300'>
-        <h2>Mina färdplaner</h2>
-        <ul>
           {userdata.authoredRoadmaps.map((authoredRoadmaps, index) => 
             <li key={index} className='margin-block-25'>
               <div className='flex justify-content-space-between align-items-center'>
                 <div>
-                  <a href={`/roadmap/${authoredRoadmaps.id}`}>{authoredRoadmaps.metaRoadmap.name}</a> <br/> {/* TODO: Check if naming of roadmap is always inherited */}
+                  <a href={`/roadmap/${authoredRoadmaps.id}`}>{authoredRoadmaps.metaRoadmap.name}</a> • <span>Färdplan</span> <br/> {/* TODO: Check if naming of roadmap is always inherited */}
                   <span>Antal målbanor: {authoredRoadmaps._count.goals}</span>
                 </div>
                 <TableMenu object={authoredRoadmaps} />
@@ -128,6 +150,7 @@ export default async function Page({ params }: { params: { user: string } }) {
           )}
         </ul>
       </section>
+
     </main>
   </>
 }
