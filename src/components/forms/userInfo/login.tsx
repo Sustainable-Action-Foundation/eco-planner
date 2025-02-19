acb7b97c967cbe771ea40ff54482e7ac6377d898
Klotarn <jesper.cejie@sustainable-action.org>
2025-02-19 15:35:07 +0100

    Add translation to login/signup forms

--- src/components/forms/userInfo/login.tsx
+++ src/components/forms/userInfo/login.tsx
@@ -4,8 +4,11 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
 import styles from '../forms.module.css'
+import dict from "./login.dict.json" assert { type: "json" };
+import { Locale } from "@/types";
+import { getClientLocale, validateDict } from "@/functions/clientLocale";
 
-function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
+function handleSubmit(event: React.ChangeEvent<HTMLFormElement>, locale: Locale) {
   event.preventDefault()
 
   const form = event.target
@@ -30,35 +33,37 @@ function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
         window.location.href = '/'
       }
     } else {
-      alert('Login failed.')
+      alert(dict.handleSubmit.loginFailed[locale])
     }
   }).catch(() => {
-    alert('Login failed.')
+    alert(dict.handleSubmit.loginFailed[locale])
   })
 }
 
 export default function Login() {
+  validateDict(dict)
+  const locale = getClientLocale()
 
   const [showPassword, setShowPassword] = useState(false)
 
   return (
     <>
-      <form onSubmit={handleSubmit} className={`${styles.padding}`}>
-        <h1 className="padding-bottom-100" style={{ borderBottom: '1px solid var(--gray-90)' }}>Logga in</h1>
+      <form onSubmit={(event: React.ChangeEvent<HTMLFormElement>) => handleSubmit(event, locale)} className={`${styles.padding}`}>
+        <h1 className="padding-bottom-100" style={{ borderBottom: '1px solid var(--gray-90)' }}>{dict.login.title[locale]}</h1>
 
         <label className="block margin-block-100">
-          Användarnamn
+          {dict.login.username.label[locale]}
           <div className="margin-block-50 padding-50 flex align-items-center gray-90 smooth focusable">
             <Image src="/icons/user.svg" alt="" width={24} height={24} />
-            <input className="padding-0 margin-inline-50" type="text" placeholder="användarnamn" name="username" required id="username" autoComplete="username" />
+            <input className="padding-0 margin-inline-50" type="text" placeholder={dict.login.username.placeholder[locale]} name="username" required id="username" autoComplete="username" />
           </div>
         </label>
 
         <label className="block margin-block-100">
-          Lösenord
+          {dict.login.password.label[locale]}
           <div className="margin-block-50 padding-50 flex align-items-center gray-90 smooth focusable">
             <Image src="/icons/password.svg" alt="" width={24} height={24} />
-            <input className="padding-0 margin-inline-50 transparent" type={showPassword ? 'text' : 'password'} placeholder="lösenord" name="password" required id="password" autoComplete="current-password" />
+            <input className="padding-0 margin-inline-50 transparent" type={showPassword ? 'text' : 'password'} placeholder={dict.login.password.placeholder[locale]} name="password" required id="password" autoComplete="current-password" />
             <button type="button" className={`${styles.showPasswordButton} grid padding-0 transparent`} onClick={() => setShowPassword(prevState => !prevState)}>
               <Image src={showPassword ? '/icons/eyeDisabled.svg' : '/icons/eye.svg'} alt="" width={24} height={24} />
             </button>
@@ -68,18 +73,18 @@ export default function Login() {
         <div className="flex gap-100 flex-wrap-wrap align-items-center justify-content-space-between">
           <label className="flex align-items-center gap-50">
             <input type="checkbox" name="remember" id="remember" />
-            Kom ihåg mig
+            {dict.login.rememberMe[locale]}
           </label>
 
-          <small><Link href='/password'>Glömt lösenordet?</Link></small>
+          <small><Link href='/password'>{dict.login.forgotPassword[locale]}</Link></small>
 
         </div>
 
-        <input type="submit" value={'Logga in'} className="block font-weight-bold seagreen smooth color-purewhite margin-top-200" />
+        <input type="submit" value={dict.login.submit.login[locale]} className="block font-weight-bold seagreen smooth color-purewhite margin-top-200" />
 
         <p className="text-align-center padding-block-50 margin-bottom-100 margin-top-0">
-          Har du inget konto? <Link href='/signup'>Skapa konto</Link> <br />
-          <Link href='/verify'>Verifiera konto</Link> {/* TODO: Flytta denna till ens account page */}
+          {dict.login.noAccount.label[locale]} <Link href='/signup'>{dict.login.noAccount.createAccount[locale]}</Link> <br />
+          <Link href='/verify'>{dict.login.noAccount.verifyAccount[locale]}</Link> {/* TODO: Flytta denna till ens account page */}
         </p>
 
 
