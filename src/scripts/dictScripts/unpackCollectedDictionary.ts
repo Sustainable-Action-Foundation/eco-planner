02b304a1da7a40b699c577cc4280d8e8af1fafe3
Klotarn <jesper.cejie@sustainable-action.org>
2025-02-26 09:48:31 +0100

    Clean up function in unpackCollectedDictionary

--- src/scripts/dictScripts/unpackCollectedDictionary.ts
+++ src/scripts/dictScripts/unpackCollectedDictionary.ts
@@ -9,39 +9,33 @@ function unpackCollectedDictionary() {
 
   // TODO - can this be improved/refactored?
   function findSubDictPaths(inDict: { [key: string]: string | object }): string[] {
-    function findSubDictPathsRecursively(inDictRec: { [key: string]: string | object }): string[] {
-      const paths: string[] = [];
-      for (const key of Object.keys(inDictRec)) {
-        // If the value is an object and the key is formatted as a file or folder name
-        if (typeof inDictRec[key] === 'object' && (key[0] == key[0].toUpperCase())) {
-          const subDictPaths = findSubDictPathsRecursively(inDictRec[key] as { [key: string]: string | object });
-          for (const subDictPath of subDictPaths) {
-            paths.push(path.join(key, subDictPath));
-          }
-        }
-        else if ((paths.includes(key) == false)) {
-          paths.push(key);
+    const paths: string[] = [];
+    for (const key of Object.keys(inDict)) {
+      // If the value is an object and the key is formatted as a file or folder name
+      if (typeof inDict[key] === 'object' && (key[0] == key[0].toUpperCase())) {
+        const subDictPaths = findSubDictPaths(inDict[key] as { [key: string]: string | object });
+        for (const subDictPath of subDictPaths) {
+          paths.push(path.join(key, subDictPath));
         }
       }
-      return paths;
-    };
-
-    let returnSubDictPaths = findSubDictPathsRecursively(inDict);
-
-    // Remove the last key from the path as it is the first key of each file
-    // This is done to only get the path (including the file name) of the sub dictionaries
-    for (let i = 0; i < returnSubDictPaths.length; i++) {
-      const splitPath: string[] = returnSubDictPaths[i].split(path.sep);
-      returnSubDictPaths[i] = splitPath.slice(0, splitPath.length - 1).join(path.sep);
+      else if ((paths.includes(key) == false)) {
+        paths.push(key);
+      }
     }
-
-    // Remove duplicates that are created by the recursive function
-    returnSubDictPaths = returnSubDictPaths.filter((item, index) => returnSubDictPaths.indexOf(item) === index)
-
-    return returnSubDictPaths;
+    return paths;
   }
 
-  const subDictPaths = findSubDictPaths(collectedDictionary as { [key: string]: string | object });
+  let subDictPaths = findSubDictPaths(collectedDictionary as { [key: string]: string | object });
+
+  // Remove the last key from the path as it is the first key of each file
+  // This is done to only get the path (including the file name) of the sub dictionaries
+  for (let i = 0; i < subDictPaths.length; i++) {
+    const splitPath: string[] = subDictPaths[i].split(path.sep);
+    subDictPaths[i] = splitPath.slice(0, splitPath.length - 1).join(path.sep);
+  }
+
+  // Remove duplicates that are created by the recursive function
+  subDictPaths = subDictPaths.filter((item, index) => subDictPaths.indexOf(item) === index)
 
   for (let filePath of subDictPaths) {
     const pathParts = filePath.split(path.sep);
