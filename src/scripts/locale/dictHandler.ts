3fa9da4018cc53da4eefe81b3d78c27f66e69e47
Klotarn <jesper.cejie@sustainable-action.org>
2025-02-27 11:31:53 +0100

    Add declarations and exports for folder and file name prefixes and suffixes to make it easier to modify how folders and files are represented in the collected dictionary.

--- src/scripts/locale/dictHandler.ts
+++ src/scripts/locale/dictHandler.ts
@@ -4,28 +4,49 @@ import path from "node:path";
 export const collectedDictionaryPath: string = path.join("src", "collectedDictionary.json");
 export const dictFileEnding: string = ".dict.json";
 
+/**
+ * The prefixes and suffixes are used decide how files
+ * and folders are represented in the packaged dictionary.
+ * They need to be able to be identified in as files and
+ * folders in the dictionary so that they can be unpacked
+ * properly.
+ * 
+ * When packaging all dictionary files, the folderstructure 
+ * is maintained by using these prefixes and suffixes to 
+ * differentiate folders and files from translation keys.
+ */
+export const [folderNamePrefix, folderNameSuffix] = ["", "-folder"];
+export const [fileNamePrefix, fileNameSuffix] = ["", "-file"];
+
+/**
+ * This class is used to convert folder and file names to keys
+ * and vice versa. The keys are used to represent the folders
+ * and files in the packaged dictionary. The keys are used to
+ * maintain the folder structure when packaging the dictionary.
+ * @public
+ */
 export class KeyNameHandler {
-  public static fileNameToKey(fileName: string): string {
-    return fileName.replace(dictFileEnding, "-file");
+  public static folderNameToKey(folderName: string): string {
+    return folderNamePrefix + folderName + folderNameSuffix;
   }
 
-  public static folderNameToKey(folderName: string): string {
-    return folderName + "-folder";
+  public static fileNameToKey(fileName: string): string {
+    return fileNamePrefix + fileName.replace(dictFileEnding, fileNameSuffix);
   }
 
   public static keyToFileOrFolderName(key: string): string {
-    if (key.endsWith("-file")) {
-      return key.replace("-file", dictFileEnding);
+    if (key.endsWith(fileNameSuffix) && key.startsWith(fileNamePrefix)) {
+      return key.replace(fileNamePrefix, "").replace(fileNameSuffix, dictFileEnding);
     }
-    return key.replace("-folder", "");
-  }
-
-  public static keyIsFileName(key: string): boolean {
-    return key.endsWith("-file");
+    return key.replace(folderNamePrefix, "").replace(folderNameSuffix, "");
   }
 
   public static keyIsFolderName(key: string): boolean {
-    return key.endsWith("-folder");
+    return key.endsWith(folderNameSuffix) && key.startsWith(folderNamePrefix);
+  }
+
+  public static keyIsFileName(key: string): boolean {
+    return key.endsWith(fileNameSuffix) && key.startsWith(fileNamePrefix);
   }
 
   public static keyIsFileOrFolderName(key: string): boolean {
