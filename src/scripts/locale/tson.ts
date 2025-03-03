0d9272db450b6e83668f66934e01e7f1470e9c24
viggoStrom <viggo.strom@sustainable-action.org>
2025-03-03 09:47:55 +0100

    update tson script

--- src/scripts/locale/tson.ts
+++ src/scripts/locale/tson.ts
@@ -18,6 +18,12 @@ const dictPaths = glob.sync(`src/**/*${fileEndings.old}`).map(file => path.resol
  */
 const leafObjectFinderRegex = /(?:"(?:.*)":)\s*(?:"[^"]*")[^:}]*}/g;
 
+const colors = {
+  red: (text: string) => `\x1b[31m${text}\x1b[0m`,
+  yellow: (text: string) => `\x1b[33m${text}\x1b[0m`,
+  gray: (text: string) => `\x1b[90m${text}\x1b[0m`,
+}
+
 const formatContent = (content: string): string => {
   return content
     .split("\n")
@@ -35,12 +41,25 @@ import { Locale } from "@/types.ts";
 export const createDict = (locale: Locale) => (${formatContent(content)});
 `.trim();
 
+let problems = false;
 
 /* Go through all files */
 dictPaths.forEach(filePath => {
-  const content = fs.readFileSync(filePath, "utf-8");
-  const newContent = templateTSON(content);
-  const newFilePath = filePath.replace(fileEndings.old, fileEndings.new);
-  fs.writeFileSync(newFilePath, newContent);
-  fs.unlink(filePath, () => { }); // Not sync version
+  try {
+    const content = fs.readFileSync(filePath, "utf-8");
+    const newContent = templateTSON(content);
+    const newFilePath = filePath.replace(fileEndings.old, fileEndings.new);
+
+    fs.renameSync(filePath, newFilePath);
+    fs.writeFileSync(newFilePath, newContent);
+  } catch (error) {
+    console.error(colors.red(`❌ Failed to convert file: ${colors.gray(filePath)}\n  ${colors.red(error as string)}\n`));
+    problems = true;
+  }
 });
+
+if (!problems) {
+  console.info("");
+  console.info("✔️  All files converted successfully!");
+  console.info("");
+}
