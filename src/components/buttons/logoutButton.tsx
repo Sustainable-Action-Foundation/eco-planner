f7e7a7ea23f54e0a22d27f4b060303c61ae61383
viggoStrom <viggo.strom@sustainable-action.org>
2025-03-17 15:07:28 +0100

    translate logout button

--- src/components/buttons/logoutButton.tsx
+++ src/components/buttons/logoutButton.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import Image from "next/image"
+import { useTranslation } from "react-i18next"
 
 export default function LogoutButton() {
+  const { t } = useTranslation()
   return (
-    <button className="flex align-items-center rounded transparent padding-50 gap-50 width-100 font-weight-500" style={{fontSize: '1rem', whiteSpace: 'nowrap'}} onClick={async () => {
+    <button className="flex align-items-center rounded transparent padding-50 gap-50 width-100 font-weight-500" style={{ fontSize: '1rem', whiteSpace: 'nowrap' }} onClick={async () => {
       fetch('/api/logout', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -17,7 +19,7 @@ export default function LogoutButton() {
       })
     }}>
       <Image src="/icons/logout.svg" alt="" width="24" height="24" />
-      Logga ut
+      {t("components:logout")}
     </button>
   )
 }
