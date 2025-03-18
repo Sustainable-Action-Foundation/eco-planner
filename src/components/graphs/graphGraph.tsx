86d16c823fd48f0070686d672b79fc9b4a468abd
Klotarn <jesper.cejie@sustainable-action.org>
2025-03-18 13:46:29 +0100

    Display full name in the information text about data source below graph

--- src/components/graphs/graphGraph.tsx
+++ src/components/graphs/graphGraph.tsx
@@ -70,7 +70,9 @@ export default function GraphGraph({
       </div>
       {historicalData && (
         <div>
-          Den historiska datan i grafen är hämtad från <Link href={externalDatasets[historicalData.metadata[0].source as keyof typeof externalDatasets].userFacingUrl} target="_blank">{historicalData.metadata[0].source}</Link>
+          Den historiska datan i grafen är hämtad från <Link href={externalDatasets[historicalData.metadata[0].source as keyof typeof externalDatasets].userFacingUrl} target="_blank">
+            {// If the data source has a full name, display the full name. Otherwise display the 
+              externalDatasets[historicalData.metadata[0].source as keyof typeof externalDatasets].fullName ? externalDatasets[historicalData.metadata[0].source as keyof typeof externalDatasets].fullName : historicalData.metadata[0].source}</Link>
         </div>
       )}
     </>
