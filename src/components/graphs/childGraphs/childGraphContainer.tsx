b9b67a967365e3342ec5449340a13860b000d53b
Leon-Loov <leon.loov@sustainable-action.org>
2025-03-31 14:34:05 +0200

    Change casing of graphSelector folder (part 1)

--- src/components/graphs/childGraphs/childGraphContainer.tsx
+++ src/components/graphs/childGraphs/childGraphContainer.tsx
@@ -6,8 +6,8 @@ import PredictionChildGraph from "./predictionChildGraph.tsx";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { getStoredChildGraphType } from "../functions/graphFunctions";
-import { percentAndFraction } from "../graphselector/graphSelector";
-import ChildGraphSelector from "../graphselector/childGraphSelector";
+import { percentAndFraction } from "../graphSelectorTemp/graphSelector.tsx";
+import ChildGraphSelector from "../graphSelectorTemp/childGraphSelector.tsx";
 
 export enum ChildGraphType {
   Target = "TARGET",
