c780b7e717d6ab5693f91ccf1fa9d74479308c69
viggoStrom <viggo.strom@sustainable-action.org>
2025-03-31 15:32:57 +0200

    update localesFormatter
    update localesFormatter
    
    update localesFormatter
    
    update localesFormatter

--- src/scripts/localesFormat.ts
+++ src/scripts/localesFormat.ts
@@ -1,5 +1,4 @@
 import "./lib/console.ts";
-import { colors } from "./lib/colors.ts";
 import fs from "node:fs";
 import path from "node:path";
 import { glob } from "glob";
@@ -7,4 +6,26 @@ import { glob } from "glob";
 const localesDir = path.join(process.cwd(), "public/locales");
 const localePaths = glob.sync(`${localesDir}/**/*.json`);
 
-console.debug(localePaths);
+localePaths.forEach(localePath => {
+  if (localePath.endsWith("common.json")) return;
+
+  const content = fs.readFileSync(localePath, "utf-8");
+
+  try { JSON.parse(content); }
+  catch (e) {
+    console.error(`Error parsing JSON in ${localePath}:`, e);
+    return;
+  }
+
+  const json = JSON.parse(content);
+
+  // Sort all root levels keys in alphabetical order
+  const sortedJson = Object.keys(json).sort().reduce((acc: Record<string, object | string>, key) => {
+    acc[key] = json[key];
+    return acc;
+  }, {});
+
+  fs.writeFileSync(localePath, JSON.stringify(sortedJson, null, 2), "utf-8");
+});
+
+console.info("Locale files formatted successfully.");
