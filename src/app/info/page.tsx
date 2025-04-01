dacb70d7ebc7861e6bb95ed0f09abe15e43f88ae
viggoStrom <viggo.strom@sustainable-action.org>
2025-04-01 14:22:58 +0200

    repair import

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -3,7 +3,7 @@ import { JSONValue } from "@/types.ts";
 import fs from "fs";
 import metadata from "package.json" with { type: "json" };
 import { t } from "@/lib/i18nServer";
-import { CommitWithLink, FallbackRemote, Intro, KnownRemote } from "@/components/info/appMetaInfo";
+import { CommitWithLink, FallbackRemote, Intro, KnownRemote } from "@/app/info/appMetaInfo";
 
 export default async function Page() {
   const gitHash = { shortHash: process.env.GIT_SHORT_HASH, longHash: process.env.GIT_LONG_HASH };
