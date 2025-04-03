4d2b29324af4313be5a4476311345b7d084305a3
viggoStrom <viggo.strom@sustainable-action.org>
2025-04-03 12:07:39 +0200

    ignore src/scripts folder on build

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,19 @@ const nextConfig = {
   experimental: {
     instrumentationHook: true,
   },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+
+    // Ignore "src/scripts" folder in the build process
+    config.module.rules.push({
+      test: /src\/scripts/,
+      use: 'ignore-loader',
+    })
+
+    return config;
+  },
 }
 
 export default nextConfig
