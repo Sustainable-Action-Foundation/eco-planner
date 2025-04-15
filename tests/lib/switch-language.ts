12d49b9fe21f7bade6df0342b168c2d537ed3d2f
viggoStrom <viggo.strom@sustainable-action.org>
2025-04-15 16:33:55 +0200

    rename language switcher helper

--- tests/lib/switch-language.ts
+++ tests/lib/switch-language.ts
@@ -1,6 +1,6 @@
 import { Page } from "playwright/test";
 
-export async function switcherLanguage(page: Page, language: string) {
+export async function switchLanguage(page: Page, language: string) {
   const languageSwitcher = page.getByTestId("language-switcher");
   languageSwitcher.selectOption(language)
   await page.waitForTimeout(1000);
--- tests/locales-page.spec.ts
+++ tests/locales-page.spec.ts
@@ -1,4 +1,4 @@
-import { switcherLanguage as switchLanguage } from "lib/switch-language";
+import { switchLanguage } from "lib/switch-language";
 import "./lib/console";
 import { expect, test } from "playwright/test";
 
