1de8d97afa10017fd5795b9a31654f09c9781a81
Axel <axel.schnurer@gmail.com>
2025-04-26 12:15:15 +0200

    Truncate title and description if too long

--- src/functions/buildMetadata.ts
+++ src/functions/buildMetadata.ts
@@ -1,9 +1,20 @@
 import { baseUrl } from "@/lib/baseUrl";
+import { re } from "mathjs";
 import { Metadata } from "next";
 
 const default_title: string = 'Eco - planner'
 const default_description: string = 'Ett verktyg som stödjer Sveriges klimatomställning genom lokala handlingsplaner, gemensam åtgärdsdatabas och samarbete kring färdplaner.'
 const default_image_path: string = '/images/solarpanels_small.jpg'
+
+// TODO: Export this function?
+// Truncates text after the end of a word
+function truncateText(string: string | null | undefined, maxLength: number): string | undefined {
+  if (!string) return
+  if (string.length <= maxLength) return string;
+  
+  const truncated = string.slice(0, maxLength);
+  return truncated.slice(0, truncated.lastIndexOf(' ')) + '…';
+}
 
 // TODO: Ensure description not longer than 150chars
 // TODO: Ensure title not to long either 
@@ -23,6 +34,11 @@ export function buildMetadata(
     image_url?: string | URL
     og_url: string | undefined
   }): Metadata {
+
+  // Truncates metadata text to fit commonly used lengths
+  title = truncateText(title, 60 - default_title.length)
+  description = truncateText(description, 150)  
+   
   return {
     title: `${title ? `${title} | ${default_title}` : default_title}`,
     description: description ?? default_description,
