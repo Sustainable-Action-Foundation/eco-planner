1022c1cc7ff24ddc076413aef4bde240f1609aba
Axelgustavschnurer <axel.schnurer@gmail.com>
2025-07-11 13:27:15 +0200

    Handle escape key

--- src/components/forms/formElements/combox.tsx
+++ src/components/forms/formElements/combox.tsx
@@ -6,13 +6,13 @@ import { useState } from "react";
 export default function Combobox() {
 
   const [value, setValue] = useState<string>('');
-  const [displayListBox, setDisplayListBox] = useState<Boolean>(false)
+  const [displayListBox, setDisplayListBox] = useState<boolean>(false)
 
   const handleKeyDownSearchInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // Always clear input and remove listbox if pressing escape
     if (e.key === 'Escape') {
-      if (displayListBox) {
-        setDisplayListBox(false)
-      }
+      setValue('')
+      setDisplayListBox(false)
     }
 
     if (e.key === 'ArrowDown') {
@@ -31,24 +31,18 @@ export default function Combobox() {
       }
     }
   };
-
-  const handleKeyDownListboxOption = (e: React.KeyboardEvent<HTMLButtonElement>) => {
-    if (e.key === 'Escape') {
-      if (displayListBox) {
-        setDisplayListBox(false)
-      }
-    }
-  };
-
+  
   return (
     <>
       <div className="flex align-items-center focusable">
         <IconSearch className="margin-left-50" />
         <input 
+          value={value}
           onChange={(e) => setValue(e.target.value)} 
           onKeyDown={handleKeyDownSearchInput}
           role="combobox" 
-          type="search" 
+          type="text" 
+          placeholder="Sök.."
         />
         <button 
           onClick={() => setDisplayListBox(!displayListBox)}
@@ -63,9 +57,9 @@ export default function Combobox() {
 
       {value || displayListBox ? 
         <ul role="listbox" id="listbox" className="margin-inline-0 padding-0">
-          <button onKeyDown={handleKeyDownListboxOption} role="option" aria-selected="false">1</button>
-          <button onKeyDown={handleKeyDownListboxOption} role="option" aria-selected="false">2</button>
-          <button onKeyDown={handleKeyDownListboxOption} role="option" aria-selected="false">3</button>
+          <button role="option" aria-selected="false">1</button>
+          <button role="option" aria-selected="false">2</button>
+          <button role="option" aria-selected="false">3</button>
         </ul>
       : null }
     </>
