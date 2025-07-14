b39993463f93941dbc67e4fd2c86d13d11fc7c0a
Axel <axel.schnurer@gmail.com>
2025-07-14 13:10:01 +0200

    Handle styling and option select using mouse

--- src/components/forms/formElements/combox.tsx
+++ src/components/forms/formElements/combox.tsx
@@ -2,11 +2,15 @@
 
 import { IconSearch, IconSelector } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
+import styles from './searchableList.module.css'
 
 export default function Combobox() {
 
-  // TODO: If element looses focus, do not display listbox
   // TODO: Handle required inputs
+  // TODO: i18n
+  
+  // TODO: Up arrow should also open listbox
+  // TODO: Re add button for opening and closing??
 
   const testArray: Array<string> = [
     'apple',
@@ -22,13 +26,19 @@ export default function Combobox() {
   const [isFocused, setIsFocused] = useState<boolean>(false); // Ensures that listbox is only visible given that combobox actually retains focus
 
   const handleKeyDownSearchInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    // Escape out of select menu if pressing escape
+    // Escape out of listbox if it is open
+    // Clear text if listbox is closed
     if (e.key === 'Escape') {
-      setFocusedListBoxItem(null)
-      setDisplayListBox(false)
+      if (displayListBox && focusedListBoxItem != null) {
+        setFocusedListBoxItem(null)
+        setDisplayListBox(false)
+      } else {
+        setValue('')
+      }
     }
 
     // Select option and remove listbox
+    // TODO: Weird that this opens listbox?
     if (e.key === 'Enter') {
       if (displayListBox && focusedListBoxItem != null) {
         setValue(testArray[focusedListBoxItem])
@@ -55,7 +65,8 @@ export default function Combobox() {
       }
     }
 
-    // Retain keyboard shortcuts when disabling 
+    // Retain keyboard shortcuts
+    // TODO: Retention of keyboard shortcuts same for down arrow?
     if (e.key === 'ArrowUp' && !e.ctrlKey && !e.shiftKey && !e.altKey && !e.metaKey) {
       if (displayListBox && focusedListBoxItem != null) {
         e.preventDefault()
@@ -71,7 +82,7 @@ export default function Combobox() {
    
 
   return (
-    <>
+    <div className="position-relative" style={{width: 'min(350px, 100%)'}}>
       <label htmlFor="combobox-control">Ange aktör</label>
       <div className="flex align-items-center focusable margin-top-25">
         <IconSearch aria-hidden="true" className="margin-left-50" width={24} height={24} style={{ minWidth: '24px' }} />
@@ -80,7 +91,7 @@ export default function Combobox() {
           onChange={(e) => setValue(e.target.value)}
           onKeyDown={handleKeyDownSearchInput}
           onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
           id="combobox-control" /* TODO: Dynamic */
           role="combobox"
           type="text"
@@ -94,27 +105,32 @@ export default function Combobox() {
         <IconSelector aria-hidden="true" width={24} height={24} style={{ minWidth: '24px' }} className="margin-right-50" />
       </div>
 
-      {(displayListBox || (value !== '' && !testArray.includes(value))) && isFocused ? (
-        <ul
-          role="listbox"
-          id="listbox" /* TODO: Dynamic */
-          aria-label="Aktörer"
-          className="margin-inline-0 padding-0"
-          style={{ listStyle: 'none' }}
-        >
-          {testArray.map((item, index) =>
-            <li
-              key={index}
-              role="option"
-              aria-selected={item === value}
-              id={`listbox-${index}`}
-              style={{ backgroundColor: index === focusedListBoxItem ? '#e0e0e0' : 'transparent', }}
-            >
-              {item}
-            </li>
-          )}
-        </ul>
-      )  : null}
-    </>
+      <ul
+        role="listbox"
+        id="listbox" /* TODO: Dynamic */
+        aria-label="Aktörer"
+        aria-hidden={!(displayListBox || (value !== '' && !testArray.includes(value))) && isFocused} // TODO: Check that this works as expected
+        className={`${styles['listbox']} margin-inline-0 padding-0`}
+        /* Setting styling insead of conditionally rendering allows us to animate using css transitions */
+        style={{    
+          pointerEvents: (displayListBox || (value !== '' && !testArray.includes(value))) && isFocused ? 'auto' : 'none',
+          opacity: (displayListBox || (value !== '' && !testArray.includes(value))) && isFocused ? 1 : 0,
+          transform: (displayListBox || (value !== '' && !testArray.includes(value))) && isFocused ? 'scale(1)' : 'scale(0.95)'
+        }}
+      >
+        {testArray.map((item, index) =>
+          <li
+            key={index}
+            role="option"
+            aria-selected={item === value}
+            id={`listbox-${index}`}
+            style={{backgroundColor: index === focusedListBoxItem ? 'var(--gray-90)' : '', }}
+            onClick={() => {setValue(item), setDisplayListBox(false)}}
+          >
+            {item}
+          </li>
+        )}
+      </ul>
+    </div>
   )
 }
