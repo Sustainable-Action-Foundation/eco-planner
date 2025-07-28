9c808d2062c67f1f12427f521e25deff9c0ebe3e
Axel <axel.schnurer@gmail.com>
2025-07-28 17:23:29 +0200

    Add max character counter

--- src/components/generic/textEditor/textEditor.tsx
+++ src/components/generic/textEditor/textEditor.tsx
@@ -29,12 +29,14 @@ import {
   LineThrough,
   Underline,
   GreyText,
-  FontSize
+  FontSize,
+  CharacterCount
 } from './extensions'
 
 export const allowedProtocols = ['http', 'https', 'mailto', 'callto', 'tel'];
+const limit = 5000
 
-const TextEditor = ({ 
+const TextEditor = ({
   ariaLabelledBy,
   placeholder
 }: {
@@ -51,6 +53,10 @@ const TextEditor = ({
       TextStyle.configure({ mergeNestedSpanStyles: true }),
       Placeholder.configure({
         placeholder: placeholder ? placeholder : undefined
+      }),
+      CharacterCount.configure({
+        limit,
+        mode: 'nodeSize'
       }),
       Highlight,
       Subscript,
@@ -89,12 +95,33 @@ const TextEditor = ({
   }
 
   /* TODO: Keyboard shortcut and custom menu for linkinput */
-  /* TODO: Max char count: 5 000chars */
+
+  const percentage = editor ? Math.round((100 / limit) * editor.storage.characterCount.characters({ mode: 'nodeSize' })) : 0
+
   return (
-    <div className='tiptap-wrapper purewhite smooth' style={{ border: '1px solid var(--gray-80)' }}>
-      <TextEditorMenu editor={editor} />
-      <EditorContent editor={editor} aria-labelledby={ariaLabelledBy} />
-    </div>
+      <div className='tiptap-wrapper purewhite smooth' style={{ border: '1px solid var(--gray-80)' }}>
+        <TextEditorMenu editor={editor} />
+        <EditorContent editor={editor} aria-labelledby={ariaLabelledBy} />
+        <div className='flex align-items-center gap-50 padding-50'>
+          <svg height="24" width="24" viewBox="0 0 20 20">
+            <circle r="10" cx="10" cy="10" fill="#e9ecef" />
+            <circle
+              r="5"
+              cx="10"
+              cy="10"
+              fill="transparent"
+              stroke={`${editor.storage.characterCount.characters({ mode: 'nodeSize' }) === limit ? '#d83545ff' : 'var(--blue-40)'}`}
+              strokeWidth="10"
+              strokeDasharray={`calc(${percentage} * 31.4 / 100) 31.4`}
+              transform="rotate(-90) translate(-20)"
+            />
+            <circle r="6" cx="10" cy="10" fill="white" />
+          </svg>
+          <small style={{fontSize: '12px'}}>
+            Karaktärer: {editor.storage.characterCount.characters()}
+          </small>
+        </div>
+      </div>
   )
 }
 
