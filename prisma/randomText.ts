81e1da76c4eacbf97463ae1460077282b416d358
Vena <viggo.strom@sustainable-action.org>
2025-08-07 11:51:21 +0200

    add random text generator for seeding

---
+++ prisma/randomText.ts
@@ -0,0 +1,76 @@
+import rawText from "./text.json" with { type: "json" };
+
+// Filtering
+const text = rawText
+  .filter(t => {
+    // Counts
+    const charCount = t.length;
+    const digitCount = (t.match(/\d/g) || []).length;
+    const nonCharCount = (t.match(/[^a-zA-Z\s]/g) || []).length;
+
+    // Filter out texts with more than 10% digits
+    if (digitCount / charCount > 0.1) return false;
+
+    // Filter out texts with more than 20% non-character symbols
+    if (nonCharCount / charCount > 0.2) return false;
+
+    // Filter less than 3 characters
+    if (charCount < 3) return false;
+
+    return true;
+  });
+
+// Group by word count
+const byWordCount: Record<number, string[]> = {};
+for (const item of text) {
+  const wordCount = item.split(/\s+/).length;
+
+  // Create length category if it doesn't exist
+  if (!byWordCount[wordCount]) byWordCount[wordCount] = [];
+
+  // Add the item to the corresponding length category
+  byWordCount[wordCount].push(item);
+}
+
+// Getters
+export class RandomTextSE {
+  static fallbackString: string = "ORD";
+
+  static sentence(wordCount: number): string {
+    const items = byWordCount[wordCount] || [];
+    if (items.length === 0 && wordCount > 1) {
+      console.info("No items found for word count", wordCount, "falling back to word count", wordCount - 1);
+      return this.sentence(wordCount - 1)
+    }
+    else if (items.length === 0) {
+      console.warn("No items found for word count", wordCount, "returning random word");
+      return this.words(1);
+    }
+    return items[Math.floor(Math.random() * items.length)];
+  }
+
+  static sentences(sentenceCount: number = 1, wordsPerSentence: number = 10): string {
+    const sentences = [];
+    for (let i = 0; i < sentenceCount; i++) {
+      sentences.push(this.sentence(wordsPerSentence));
+    }
+    return sentences.join(" ");
+  }
+
+  static word(): string {
+    return this.words(1);
+  }
+
+  static words(wordCount: number = 1): string {
+    const items = byWordCount[wordCount] || [];
+    if (items.length === 0 && wordCount > 1) {
+      console.info("No items found for word count", wordCount, "falling back to word count", wordCount - 1);
+      return this.words(wordCount - 1);
+    }
+    else if (items.length === 0) {
+      console.warn("No items found for word count", wordCount, `returning ${this.fallbackString} in place of words`);
+      return this.fallbackString.repeat(wordCount);
+    }
+    return items[Math.floor(Math.random() * items.length)];
+  }
+}
