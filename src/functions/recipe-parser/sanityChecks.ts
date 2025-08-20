3c1ee144a4bc532c8dddc9e80cdfa0ab583ac01a
Noëlle <leon.loov@sustainable-action.org>
2025-08-20 11:12:23 +0200

    fix sanity checks for data series in recipes

--- src/functions/recipe-parser/sanityChecks.ts
+++ src/functions/recipe-parser/sanityChecks.ts
@@ -24,19 +24,25 @@ export function sketchyScalars(scalars: EvalTimeScalar[], warnings: string[]) {
 }
 
 export function sketchyDataSeries(dataSeries: EvalTimeDataSeries[], warnings: string[]) {
-  const hugeValuesInDataSeries = dataSeries.filter(variable =>
-    Object.values(variable.matrix).some(v => !isNull(v) && v !== null && Math.abs(Number(v)) > 1e12)
-  );
+  const hugeValuesInDataSeries = dataSeries.filter(variable => {
+    if (typeof variable.value === "number") {
+      return Math.abs(variable.value) > 1e12;
+    } else if (Array.isArray(variable.value)) {
+      return variable.value?.some(v => !isNull(v) && v !== null && Math.abs(Number(v)) > 1e12)
+    } else {
+      return false;
+    }
+  });
   if (hugeValuesInDataSeries.length > 0) {
     warnings.push(`Recipe contains data series with huge values: ${hugeValuesInDataSeries.map(ds => ds.name).join(", ")}, which may lead to performance issues or overflow errors.`);
   }
 
-  const longDataSeries = dataSeries.filter(variable => Object.keys(variable.matrix).length > 50);
+  const longDataSeries = dataSeries.filter(variable => Array.isArray(variable.value) && variable.value.length > 50);
   if (longDataSeries.length > 0) {
     warnings.push(`Recipe contains very long data series: ${longDataSeries.map(ds => ds.name).join(", ")}, which may lead to performance issues or unexpected results in calculations.`);
   }
 
-  const shortDataSeries = dataSeries.filter(variable => Object.keys(variable.matrix).length < 2);
+  const shortDataSeries = dataSeries.filter(variable => Array.isArray(variable.value) && variable.value.length < 2);
   if (shortDataSeries.length > 0) {
     warnings.push(`Recipe contains very short data series: ${shortDataSeries.map(ds => ds.name).join(", ")}, which may lead to unexpected results in calculations.`);
   }
