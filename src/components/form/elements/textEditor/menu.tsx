3b83888741c4d0c0e7256a7cf632d6dc5af8385b
Axelgustavschnurer <axel.schnurer@gmail.com>
2025-08-25 22:36:21 +0200

    Render menubuttons based on breakpoints

--- src/components/form/elements/textEditor/menu.tsx
+++ src/components/form/elements/textEditor/menu.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useTranslation } from "react-i18next";
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Editor } from "@tiptap/core";
 import styles from './textEditor.module.css' with { type: "css" }
 import { BulletList, Link, NumberedList, Highlight, Subscript, Superscript, Underline, StrikeThrough, Bold, Italic, GreyText, FontSize, Redo, Undo } from "./menuItems";
@@ -18,58 +18,60 @@ export default function TextEditorMenu({
   const { t } = useTranslation("components");
 
   const [focusedMenubarItem, setFocusedMenubarItem] = useState<number | null>(null);
-  const initialList = [
-    <li role='presentation' key="undo">
-      <Undo editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={1} />
-    </li>,
-    <li role='presentation' className={`margin-right-25 padding-right-25 ${styles['divider']}`} key="redo">
-      <Redo editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={1} />
-    </li>,
-    <li role='presentation' className={`margin-right-25 padding-right-25 position-relative ${styles['divider']}`} key="font-size">
-      <FontSize editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} editorId={editorId} menuGroup={2} />
-    </li>,
-    <li role='presentation' key="grey-text">
-      <GreyText editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
-    </li>,
-    <li role='presentation' key="italic">
-      <Italic editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
-    </li>,
-    <li role='presentation' key="bold">
-      <Bold editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
-    </li>,
-    <li role='presentation' key="strike-through">
-      <StrikeThrough editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
-    </li>,
-    <li role='presentation' key="underline">
-      <Underline editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
-    </li>,
-    <li role='presentation' key="superscript">
-      <Superscript editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
-    </li>,
-    <li role='presentation' key="subscript">
-      <Subscript editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
-    </li>,
-    <li role='presentation' className={`margin-right-25 padding-right-25 ${styles['divider']}`} key="highlight">
-      <Highlight editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
-    </li>,
-    <li role='presentation' className={`margin-right-25 padding-right-25 ${styles['divider']}`} key="link">
-      <Link editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={4} />
-    </li>,
-    <li role='presentation' key="bulletlist" >
-      <BulletList editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={5} />
-    </li>,
-    <li role='presentation' key="numberedlist" >
-      <NumberedList editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={5} />
-    </li>
-  ]
-
-  const [ listItems, setListItems ] = useState<React.JSX.Element[]>(initialList)
-  const [ hiddenListItems, setHiddenListItems ] = useState<React.JSX.Element[]>([])
+  const [menuBarParentWidth, setMenuBarParentWidth] = useState<number | undefined>(undefined); // width of menubar parent, updated on resize
+  const [visibleGroups, setVisibleGroups] = useState<number[]>() // Groups which should be shown in the menubar 
+  const [hiddenGroups, setHiddenGroups] = useState<number[]>() // Groups which should be shown in an expandable menu
 
   const menubarRef = useRef<HTMLUListElement | null>(null);
   const menuItemsRef = useRef<NodeListOf<HTMLElement> | null>(null);
-  const breakpointsRef = useRef<Record<string, number>>({});
+  const breakpointsRef = useRef<Record<string, number>>({}); // Breakpoints calculated from width of menu elements, calculated once on render.
+ 
+  const menuItemsList = [
+    <li role='presentation' data-menu-group={1} key="undo">
+      <Undo editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={1} />
+    </li>,
+    <li role='presentation' data-menu-group={1} className={`margin-right-25 padding-right-25 ${styles['divider']}`} key="redo">
+      <Redo editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={1} />
+    </li>,
+    <li role='presentation' data-menu-group={2} className={`margin-right-25 padding-right-25 position-relative ${styles['divider']}`} key="font-size">
+      <FontSize editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} editorId={editorId} menuGroup={2} />
+    </li>,
+    <li role='presentation' data-menu-group={3} key="grey-text">
+      <GreyText editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
+    </li>,
+    <li role='presentation' data-menu-group={3} key="italic">
+      <Italic editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
+    </li>,
+    <li role='presentation' data-menu-group={3} key="bold">
+      <Bold editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
+    </li>,
+    <li role='presentation' data-menu-group={3} key="strike-through">
+      <StrikeThrough editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
+    </li>,
+    <li role='presentation' data-menu-group={3} key="underline">
+      <Underline editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
+    </li>,
+    <li role='presentation' data-menu-group={3} key="superscript">
+      <Superscript editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
+    </li>,
+    <li role='presentation' data-menu-group={3} key="subscript">
+      <Subscript editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
+    </li>,
+    <li role='presentation' data-menu-group={3} className={`margin-right-25 padding-right-25 ${styles['divider']}`} key="highlight">
+      <Highlight editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={3} />
+    </li>,
+    <li role='presentation' data-menu-group={4} className={`margin-right-25 padding-right-25 ${styles['divider']}`} key="link">
+      <Link editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={4} />
+    </li>,
+    <li role='presentation' data-menu-group={5} key="bulletlist" >
+      <BulletList editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={5} />
+    </li>,
+    <li role='presentation' data-menu-group={5} key="numberedlist" >
+      <NumberedList editor={editor} setFocusedMenubarItem={setFocusedMenubarItem} t={t} menuGroup={5} />
+    </li>
+  ];
 
+  // Get a ref of all menu items in our menubar
   useEffect(() => {
     if (menubarRef.current) {
       menuItemsRef.current = menubarRef.current.querySelectorAll(
@@ -78,6 +80,80 @@ export default function TextEditorMenu({
     }
   }, [])
 
+  // Calculate breakpoints in our menubar
+  useEffect(() => {
+    if (!menuItemsRef.current) return;
+
+    const groupWidths: Record<string, number> = {};
+    menuItemsRef.current.forEach((menuItem) => {
+      const menuItemGroup = menuItem.dataset.menuGroup;
+      const menuItemParent = menuItem.parentElement;
+      if (!menuItemParent || !menuItemGroup) return;
+
+      const width =
+        menuItemParent.offsetWidth +
+        parseInt(getComputedStyle(menuItemParent).marginRight);
+      groupWidths[menuItemGroup] =
+        (groupWidths[menuItemGroup] ?? 0) +
+        width;
+    })
+
+    const breakpoints: Record<string, number> = {};
+    let runningTotal = 0;
+
+    Object.keys(groupWidths)
+      .forEach((key) => {
+        runningTotal += groupWidths[key];
+        breakpoints[key] = runningTotal;
+      });
+
+    breakpointsRef.current = breakpoints;
+  }, [])
+
+  // Add resize observer to our menubar
+  useEffect(() => {
+    function updateWidth() {
+      const menuBarParent = menubarRef.current?.parentElement
+      if (!menuBarParent) return
+
+      setMenuBarParentWidth(
+        menuBarParent.clientWidth -
+        parseInt(getComputedStyle(menuBarParent).paddingInline)
+      );
+    }
+
+    updateWidth();
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  }, [])
+ 
+  
+  useEffect(() => {
+    if (!menuBarParentWidth) return
+    
+    const calculatedVisibleGroups: number[] = [];
+    const calculatedHiddenGroups: number[] = [];
+
+    menuItemsList.map((menuItem) => {
+      if (menuBarParentWidth < breakpointsRef.current[menuItem.props["data-menu-group"]]) {
+        if (!calculatedHiddenGroups.includes(menuItem.props["data-menu-group"])) {
+          calculatedHiddenGroups.push(menuItem.props["data-menu-group"])
+        }
+      } else {
+        if (!calculatedVisibleGroups.includes(menuItem.props["data-menu-group"])) {
+          calculatedVisibleGroups.push(menuItem.props["data-menu-group"])
+        }
+      }
+    })
+
+    console.log(calculatedVisibleGroups)
+ 
+    setVisibleGroups(calculatedVisibleGroups)
+    setHiddenGroups(calculatedHiddenGroups)
+
+  }, [menuBarParentWidth])
+
+  // Set focus to a menubar item when navigating using keyboard arrows
   useEffect(() => {
     if (!menuItemsRef.current) return;
 
@@ -90,52 +166,6 @@ export default function TextEditorMenu({
     }
   }, [focusedMenubarItem]);
 
-  useEffect(() => {
-    if (!menuItemsRef.current) return;
- 
-    const groupWidths: Record<string, number> = {};
-    menuItemsRef.current.forEach((menuItem) => {
-      const menuItemGroup = menuItem.dataset.menuGroup;
-      const menuItemParent = menuItem.parentElement;
-      if (!menuItemParent || !menuItemGroup) return;
-
-      const width = 
-        menuItemParent.offsetWidth + 
-        parseInt(getComputedStyle(menuItemParent).marginRight);
-      groupWidths[menuItemGroup] = 
-        (groupWidths[menuItemGroup] ?? 0) +
-        width;
-    })
-    
-    const breakpoints: Record<string, number> = {};
-    let runningTotal = 0;
-
-    Object.keys(groupWidths)
-      .forEach((key) => {
-        runningTotal += groupWidths[key];
-        breakpoints[key] = runningTotal;
-      });
-
-  breakpointsRef.current = breakpoints;  
-  }, []) 
-
-  const [menuBarParentWidth, setMenuBarParentWidth] = useState<number | undefined>(undefined);
-  useEffect(() => {
-    function updateWidth() {
-      const menuBarParent = menubarRef.current?.parentElement
-      if (!menuBarParent) return
-
-      setMenuBarParentWidth(
-        menuBarParent.clientWidth - 
-        parseInt(getComputedStyle(menuBarParent).paddingInline)
-      ); 
-    }
-
-    updateWidth();
-    window.addEventListener("resize", updateWidth);
-    return () => window.removeEventListener("resize", updateWidth);
-  }, []) 
-
   if (!editor) {
     return null
   }
@@ -144,7 +174,7 @@ export default function TextEditorMenu({
     <div className={`${styles["text-editor-menu"]} button-group margin-0`}>
       <ul
         onKeyDown={(e: React.KeyboardEvent<HTMLUListElement>) => {
-          if (!menuItemsRef.current) return; 
+          if (!menuItemsRef.current) return;
           if (e.key === "Escape") {
             editor.commands.focus();
           }
@@ -159,9 +189,12 @@ export default function TextEditorMenu({
         role='menubar'
         className='margin-0 padding-0'
       >
-        {listItems.map((listItem) => {
-          return listItem
-        })}
+      {menuItemsList
+        .filter((menuItem) => 
+          !visibleGroups || visibleGroups.includes(Number(menuItem.props["data-menu-group"]))
+        )
+        .map((menuItem) => menuItem)
+      }
       </ul>
     </div>
   )
