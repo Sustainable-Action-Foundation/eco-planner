71167ef8ffc9f3cff8ffbf76f4a3947ae26e0b84
Vena <viggo.strom@sustainable-action.org>
2025-08-26 15:46:20 +0200

    update data series prep to allow for DataSeriesValueFields objects

--- src/app/api/goal/dataSeriesPrep.ts
+++ src/app/api/goal/dataSeriesPrep.ts
@@ -1,4 +1,4 @@
-import { DataSeriesValueFields, isFullDataSeriesValueFields, Years } from "@/types";
+import { DataSeriesValueFields, isFullDataSeriesValueFields, isPartialDataSeriesValueFields, Years } from "@/types";
 
 /**
  * Parses an object containing a string array called dataSeries into the format needed to create a data series.
@@ -8,28 +8,53 @@ import { DataSeriesValueFields, isFullDataSeriesValueFields, Years } from "@/typ
  * @returns an object containing the data fields (the ones prefixed with `val`) in a DataSeries or `null`
  */
 export default function dataSeriesPrep(
-  dataSeries: string[],
+  dataSeries: Partial<DataSeriesValueFields> | string[],
 ) {
-  // Text fields
-  const dataValues: Partial<DataSeriesValueFields> = {};
-  // Data value fields
-  if (dataSeries?.length && dataSeries.length <= Years.length) {
-    if (dataSeries.length < Years.length) {
-      const oldLength = dataSeries.length;
-      dataSeries.length = Years.length;
-      dataSeries.fill("", oldLength);
+  const cleanedDataSeries: Partial<DataSeriesValueFields> = {};
+
+  // If the data series is already a DataSeriesValueFields object, clean it and return it
+  if (!Array.isArray(dataSeries) && isPartialDataSeriesValueFields(dataSeries)) {
+    for (const year of Years) {
+        cleanedDataSeries[year] = dataSeries[year] ?? null;
     }
+    if (!isFullDataSeriesValueFields(cleanedDataSeries)) {
+      console.error("Failed to transform data series into a full DataSeriesValueFields object in dataSeriesPrep");
+      return null;
+    }
+    return cleanedDataSeries;
+  }
+
+  if (!Array.isArray(dataSeries) || !dataSeries.length) {
+    console.error("Data series is not a valid array in dataSeriesPrep");
+    return null;
+  }
+
+  if (dataSeries.length > Years.length) {
+    console.error("Data series length exceeds expected length in dataSeriesPrep");
+    return null;
+  }
+
+  // Convert string array to DataSeriesValueFields object
+  if (dataSeries.length <= Years.length) {
+    const dataSeriesCopy = [...dataSeries];
+
+    if (dataSeriesCopy.length < Years.length) {
+      const oldLength = dataSeriesCopy.length;
+      dataSeriesCopy.length = Years.length;
+      dataSeriesCopy.fill("", oldLength);
+    }
+
     // The keys for the data values are `val2020`, `val2021`, etc. up to `val2050`
-    const keys = dataSeries.map((_, index) => Years[index]);
+    const keys = dataSeriesCopy.map((_, index) => Years[index]);
     keys.forEach((key, index) => {
-      let value: number | null = parseFloat(dataSeries[index]);
+      let value: number | null = parseFloat(dataSeriesCopy[index]);
       // If the value is empty, infinite, or NaN, set it to null
-      if (dataSeries[index] == null || !Number.isFinite(value)) {
+      if (dataSeriesCopy[index] == null || !Number.isFinite(value)) {
         value = null;
       }
       // If the value is a number or null, add it to the dataValues object
       if (value === null || Number.isFinite(value)) {
-        dataValues[key] = value;
+        cleanedDataSeries[key] = value;
       }
     });
   }
@@ -39,10 +64,10 @@ export default function dataSeriesPrep(
   }
 
   // Type guard it into a non partial DataSeriesValueFields
-  if (!isFullDataSeriesValueFields(dataValues)) {
+  if (!isFullDataSeriesValueFields(cleanedDataSeries)) {
     console.error("Failed to transform data series into a full DataSeriesValueFields object in dataSeriesPrep");
     return null;
   }
 
-  return dataValues;
+  return cleanedDataSeries;
 }
