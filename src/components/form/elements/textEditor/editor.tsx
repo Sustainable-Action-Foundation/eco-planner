c920395868a6c2ab009cd0e01aa8c6a2fd11430e
Axelgustavschnurer <axel.schnurer@gmail.com>
2025-08-26 21:04:16 +0200

    Fix charactercounter firefox bug

--- src/components/form/elements/textEditor/editor.tsx
+++ src/components/form/elements/textEditor/editor.tsx
@@ -54,10 +54,12 @@ const TextEditor = ({
   /* TODO: Character counter i18n */
 
   const percentage = editor ? Math.round((100 / nodeSizeLimit) * editor.storage.characterCount.characters({ mode: 'nodeSize' })) : 0
+  const circumference = 2 * Math.PI * 5; // r = 5
+  const dash = (percentage / 100) * circumference;
 
   return (
     <div 
-      className={`${className ? `${className} ` : ''}${defaultStyles ? 'tiptap-wrapper purewhite smooth' : ''}`}
+      className={`${className ? `${className} ` : ''}${defaultStyles ? 'tiptap-wrapper purewhite smooth relative' : ''}`}
       style={{ ...style, border:`${defaultStyles ? '1px solid var(--gray-80)' : ''}` }}
     >
       {defaultStyles ? 
@@ -75,7 +77,7 @@ const TextEditor = ({
               fill="transparent"
               stroke={`${editor.storage.characterCount.characters({ mode: 'nodeSize' }) === nodeSizeLimit ? '#d83545ff' : 'var(--blue-40)'}`}
               strokeWidth="10"
-              strokeDasharray={`calc(${percentage} * 31.4 / 100) 31.4`}
+              strokeDasharray={`${dash} ${circumference}`}
               transform="rotate(-90) translate(-20)"
             />
             <circle r="6" cx="10" cy="10" fill="white" />
