63e80e2184a32878d5ac875e05592bb30517876c
Noëlle <leon.loov@sustainable-action.org>
2025-08-26 12:53:34 +0200

    add client-only

--- src/components/tables/tableSelector/tableSelector.tsx
+++ src/components/tables/tableSelector/tableSelector.tsx
@@ -1,10 +1,9 @@
-"use client"
-
+import 'client-only';
 import React, { ChangeEvent, Dispatch, SetStateAction } from 'react';
 import RadioImage from './radioImage';
 import { ViewMode } from '../goals';
 import { setStoredViewMode } from '../functions/tableFunctions';
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 import { IconList, IconListTree, IconTableFilled } from '@tabler/icons-react';
 
 export default function TableSelector({ id, current, setter }: { id: string, current: ViewMode | "", setter: Dispatch<SetStateAction<ViewMode | "">> }) {
@@ -25,15 +24,15 @@ export default function TableSelector({ id, current, setter }: { id: string, cur
     <div className='flex align-items-center gap-25'>
       <RadioImage value={ViewMode.Tree} name="table" checked={current == ViewMode.Tree} onChange={handleRadioChange}>
         {t("components:table_selector.tree")}
-        <IconListTree  aria-hidden="true" style={{minWidth: '24px'}} />
+        <IconListTree aria-hidden="true" style={{ minWidth: '24px' }} />
       </RadioImage>
       <RadioImage value={ViewMode.Table} name="table" checked={current == ViewMode.Table} onChange={handleRadioChange}>
         {t("components:table_selector.table")}
-        <IconTableFilled aria-hidden="true" style={{minWidth: '24px'}}/>
+        <IconTableFilled aria-hidden="true" style={{ minWidth: '24px' }} />
       </RadioImage>
       <RadioImage value={ViewMode.Actions} name="table" checked={current == ViewMode.Actions} onChange={handleRadioChange}>
         {t("components:table_selector.actions")}
-        <IconList aria-hidden="true" style={{minWidth: '24px'}}/>
+        <IconList aria-hidden="true" style={{ minWidth: '24px' }} />
       </RadioImage>
     </div>
   );
