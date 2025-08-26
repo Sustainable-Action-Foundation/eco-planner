b715d9a610ea240fee4a711cf18cf3a2257d1207
Axelgustavschnurer <axel.schnurer@gmail.com>
2025-08-26 16:22:00 +0200

    Cleanup some code

--- src/components/form/elements/textEditor/menu.tsx
+++ src/components/form/elements/textEditor/menu.tsx
@@ -15,17 +15,20 @@ export default function TextEditorMenu({
   editor: Editor,
   editorId: string
 }) {
-  // TODO: Keyboard controls act strange if i resize.
-
   const { t } = useTranslation("components");
 
   const [focusedMenubarItem, setFocusedMenubarItem] = useState<number | null>(null);
-  const [menuBarParentWidth, setMenuBarParentWidth] = useState<number | undefined>(undefined); // width of menubar parent, updated on resize
+  const [focusedSubmenuItem, setfocusedSubmenuItem] = useState<number | null>(null);
+  const [submenuVisible, setSubmenuVisible] = useState<boolean>()
+  const [menuBarWidth, setMenuBarWidth] = useState<number | undefined>(undefined); // width of menubar parent, updated on resize
   const [visibleGroups, setVisibleGroups] = useState<number[]>() // Groups which should be shown in the menubar 
   const [hiddenGroups, setHiddenGroups] = useState<number[]>() // Groups which should be shown in an expandable menu
 
   const menubarRef = useRef<HTMLUListElement | null>(null);
   const menuItemsRef = useRef<NodeListOf<HTMLElement> | null>(null);
+  const submenuButtonRef = useRef<HTMLSpanElement>(null);
+  const submenuRef = useRef<HTMLUListElement | null>(null);
+  const submenuItemsRef = useRef<NodeListOf<HTMLElement> | null>(null);
   const breakpointsRef = useRef<Record<string, number>>({}); // Breakpoints calculated from width of menu elements, calculated once on render.
 
   const menuItemsList = [
@@ -74,15 +77,44 @@ export default function TextEditorMenu({
   ];
 
   // Get a ref of all menu items in our menubar
-  // TODO: This is a lil hacky
-  // TODO: Should this be a ref even? Or state?
   useEffect(() => {
     if (menubarRef.current) {
       menuItemsRef.current = menubarRef.current.querySelectorAll(
         "div > [role='menubar'] > li > [role='menuitem'], div > [role='menubar'] > li > [role='menuitemcheckbox'], div > [role='menubar'] > li > [role='menuitemradio']"
       ) as NodeListOf<HTMLElement>;
     }
-  }, [menuBarParentWidth, visibleGroups, hiddenGroups])
+  }, [menuBarWidth, visibleGroups, hiddenGroups])
+
+  // Set focus to a menubar item when navigating using keyboard arrows
+  useEffect(() => {
+    if (!menuItemsRef.current) return;
+    if (focusedMenubarItem !== null) {
+      const target = menuItemsRef.current[focusedMenubarItem] as HTMLElement | undefined;
+      if (target) {
+        target.focus();
+      }
+    }
+  }, [focusedMenubarItem]);
+
+  // Get a ref of all menu items in our submenu
+  useEffect(() => {
+    if (submenuRef.current) {
+      submenuItemsRef.current = submenuRef.current.querySelectorAll(
+        "li > [role='menuitem'], li > [role='menuitemcheckbox'], li > [role='menuitemradio']"
+      ) as NodeListOf<HTMLElement>;
+    }
+  }, [submenuVisible])
+
+  // Set focus to a submenubar item when navigating using keyboard arrows
+  useEffect(() => {
+    if (!submenuItemsRef.current) return;
+    if (focusedSubmenuItem !== null) {
+      const target = submenuItemsRef.current[focusedSubmenuItem] as HTMLElement | undefined;
+      if (target) {
+        target.focus();
+      }
+    }
+  }, [focusedSubmenuItem]);
 
   // Calculate breakpoints in our menubar
   useEffect(() => {
@@ -115,15 +147,14 @@ export default function TextEditorMenu({
   }, [])
 
   // Add resize observer to our menubar
-  // TODO: Can probably just check against the menubar itself now due to changed styling.
   useEffect(() => {
     function updateWidth() {
-      const menuBarParent = menubarRef.current?.parentElement
-      if (!menuBarParent) return
+      if (!menubarRef.current) return
 
-      setMenuBarParentWidth(
-        menuBarParent.clientWidth -
-        parseInt(getComputedStyle(menuBarParent).paddingInline)
+      setMenuBarWidth(
+        menubarRef.current.clientWidth -
+        parseInt(getComputedStyle(menubarRef.current).paddingInline) -
+        48 // Width of menu element (multiplied by 2 to allow for some breathing space)
       );
     }
 
@@ -132,15 +163,15 @@ export default function TextEditorMenu({
     return () => window.removeEventListener("resize", updateWidth);
   }, [])
 
-  // TODO: Figure that one pixel which causes overflow instead of removing element from list ):
+  // Checks which groups should be visible by comparing menubar width to breakpoints
   useEffect(() => {
-    if (!menuBarParentWidth) return
+    if (!menuBarWidth) return
 
     const calculatedVisibleGroups: number[] = [];
     const calculatedHiddenGroups: number[] = [];
 
     menuItemsList.map((menuItem) => {
-      if (menuBarParentWidth < breakpointsRef.current[menuItem.props["data-menu-group"]]) {
+      if (menuBarWidth < breakpointsRef.current[menuItem.props["data-menu-group"]]) {
         if (!calculatedHiddenGroups.includes(menuItem.props["data-menu-group"])) {
           calculatedHiddenGroups.push(menuItem.props["data-menu-group"])
         }
@@ -154,56 +185,7 @@ export default function TextEditorMenu({
     setVisibleGroups(calculatedVisibleGroups)
     setHiddenGroups(calculatedHiddenGroups)
 
-  }, [menuBarParentWidth])
-
-  // Set focus to a menubar item when navigating using keyboard arrows
-  useEffect(() => {
-    if (!menuItemsRef.current) return;
-
-    if (focusedMenubarItem !== null) {
-      const target = menuItemsRef.current[focusedMenubarItem] as HTMLElement | undefined;
-
-      if (target) {
-        target.focus();
-      }
-    }
-
-  }, [focusedMenubarItem]);
-
-  // Submenu config stuff
-  const [focusedSubmenuItem, setfocusedSubmenuItem] = useState<number | null>(null);
-  const [submenuVisible, setSubmenuVisible] = useState<boolean>()
-
-  const submenuButtonRef = useRef<HTMLSpanElement>(null);
-  const submenuRef = useRef<HTMLUListElement | null>(null);
-  const submenuItemsRef = useRef<NodeListOf<HTMLElement> | null>(null);
-
-  // Get submenu items
-  useEffect(() => {
-    if (submenuRef.current) {
-      submenuItemsRef.current = submenuRef.current.querySelectorAll(
-        "li > [role='menuitem'], li > [role='menuitemcheckbox'], li > [role='menuitemradio']"
-      ) as NodeListOf<HTMLElement>;
-    }
-  }, [submenuVisible])
-
-  // Set focus to a submenubar item when navigating using keyboard arrows
-  useEffect(() => {
-    if (!submenuItemsRef.current) return;
-
-    if (focusedSubmenuItem !== null) {
-      const target = submenuItemsRef.current[focusedSubmenuItem] as HTMLElement | undefined;
-
-      if (target) {
-        target.focus();
-      }
-    }
-
-  }, [focusedSubmenuItem]);
-
-  useEffect(() => {
-    console.log(focusedMenubarItem)
-  }, [focusedMenubarItem])
+  }, [menuBarWidth])
 
   if (!editor) {
     return null
@@ -212,6 +194,8 @@ export default function TextEditorMenu({
   return (
     <div className={`${styles["text-editor-menu"]} button-group margin-0`}>
       <ul
+        className='margin-0 padding-0'
+        ref={menubarRef}
         onKeyDown={(e: React.KeyboardEvent<HTMLUListElement>) => {
           if (!menuItemsRef.current) return;
           if (e.key === "Escape") {
@@ -224,9 +208,7 @@ export default function TextEditorMenu({
             setFocusedMenubarItem
           )
         }}
-        ref={menubarRef}
         role='menubar'
-        className='margin-0 padding-0'
       >
         {menuItemsList
           .filter((menuItem) =>
@@ -243,11 +225,6 @@ export default function TextEditorMenu({
           }}>
           <span
             ref={submenuButtonRef}
-            data-tooltip="Meny" // TODO: I18n 
-            role='menuitem'
-            aria-label="Meny" // TODO: I18n
-            aria-haspopup="menu"
-            aria-checked={submenuVisible}
             tabIndex={-1}
             onClick={() => setSubmenuVisible(!submenuVisible)}
             onKeyDown={(e: React.KeyboardEvent) => {
@@ -274,10 +251,29 @@ export default function TextEditorMenu({
                 setfocusedSubmenuItem(0)
               }
             }}
+            role='menuitem'
+            aria-label="Meny" // TODO: I18n
+            aria-haspopup="menu"
+            aria-checked={submenuVisible}
+            data-tooltip="Meny" // TODO: I18n 
           >
             <IconDotsVertical className="grid" height={16} width={16} aria-hidden="true" />
           </span>
           <ul
+            style={{
+              padding: '2px',
+              width: 'max-content',
+              zIndex: '1',
+              margin: '0',
+              backgroundColor: 'var(--gray-95)',
+              borderRadius: '.25rem',
+              listStyle: 'none',
+              position: 'absolute',
+              top: 'calc(100% + 9px)',
+              right: '.25rem',
+              boxShadow: "rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px"
+            }}
+            ref={submenuRef}
             onKeyDown={(e: React.KeyboardEvent<HTMLUListElement>) => {
               if (!submenuItemsRef.current) return
               if (e.key === "Escape") {
@@ -300,21 +296,7 @@ export default function TextEditorMenu({
                 setfocusedSubmenuItem
               )
             }}
-            ref={submenuRef}
             role="menubar"
-            style={{
-              padding: '2px',
-              width: 'max-content',
-              zIndex: '1',
-              margin: '0',
-              backgroundColor: 'var(--gray-95)',
-              borderRadius: '.25rem',
-              listStyle: 'none',
-              position: 'absolute',
-              top: 'calc(100% + 9px)',
-              right: '.25rem',
-              boxShadow: "rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px"
-            }}
           >
             {submenuVisible && menuItemsList
               .filter((menuItem) =>
