22fb8a24f3f3c7c7867b318231a0b0f8136bd13b
Axelgustavschnurer <axel.schnurer@gmail.com>
2025-10-12 23:50:11 +0200

    create select-two css styles

--- src/components/form/forms/goal.tsx
+++ src/components/form/forms/goal.tsx
@@ -267,9 +267,16 @@ export default function GoalForm({
 
         </fieldset>
 
-        {/* Data series type selection (static, inherited, combined) */}
+        {/* Data series type selection (static, inherited, combined) 
         <fieldset className={`${styles.timeLineFieldset} width-100 ${positionIndex > 1 ? "margin-top-200" : ""}`}>
           <legend data-position={positionIndex++} className={`${styles.timeLineLegend} padding-block-125 font-weight-bold`}>{t("forms:goal.data_series_type_legend")}</legend>
+
+        </fieldset>*/}
+
+        {/* Data series input section (varies by type) */}
+        <fieldset className={`${styles.timeLineFieldset} width-100 margin-top-200`}>
+          <legend data-position={positionIndex++} className={`${styles.timeLineLegend} padding-block-125 font-weight-bold`}>{t("forms:goal.choose_goal_data_series")}</legend>
+
           <label>
             {t("forms:goal.data_series_type_label")}
             <select name="dataSeriesType" id="dataSeriesType" className="margin-top-25 margin-bottom-100 block" required
@@ -278,14 +285,23 @@ export default function GoalForm({
             >
               <option value={DataSeriesType.Static}>{t("forms:goal.data_series_types.static")}</option>
               <option value={DataSeriesType.Inherited}>{t("forms:goal.data_series_types.inherited")}</option>
-              <option value={DataSeriesType.Combined}>{t("forms:goal.data_series_types.combined")}</option>
+              {/* <option value={DataSeriesType.Combined}>{t("forms:goal.data_series_types.combined")}</option> TODO: Remove this? */}
             </select>
           </label>
-        </fieldset>
 
-        {/* Data series input section (varies by type) */}
-        <fieldset className={`${styles.timeLineFieldset} width-100 margin-top-200`}>
-          <legend data-position={positionIndex++} className={`${styles.timeLineLegend} padding-block-125 font-weight-bold`}>{t("forms:goal.choose_goal_data_series")}</legend>
+          <fieldset className="fieldset-unset-pseudo-class" style={{width: "400px"}}>
+            <legend>{t("forms:goal.data_series_type_label")}</legend>  {/* TODO: there is a better label for this */}
+            <div className="select-two">
+              <label>
+                {t("forms:goal.data_series_types.inherited")}
+                <input checked={dataSeriesType === DataSeriesType.Inherited} onChange={(e) => setDataSeriesType(e.target.value as DataSeriesType)} value={DataSeriesType.Inherited} type="radio" name="alternative" required /> 
+              </label>
+              <label>
+                {t("forms:goal.data_series_types.static")}
+                <input checked={dataSeriesType === DataSeriesType.Static} onChange={(e) => setDataSeriesType(e.target.value as DataSeriesType)} value={DataSeriesType.Static} type="radio" name="alternative" required />  {/* TODO: update name */}
+              </label>
+            </div> 
+          </fieldset>
 
           {(dataSeriesType === DataSeriesType.Static || !dataSeriesType) &&
             <ManualGoalForm currentGoal={currentGoal} dataSeriesString={dataSeriesString} />
