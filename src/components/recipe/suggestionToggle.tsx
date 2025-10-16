f2e313bb31faecc89d5c6ace55678c1e26534007
Noëlle <leon.loov@sustainable-action.org>
2025-10-16 15:18:20 +0200

    Change suggestionToggle to use built-in Intl disjunction formatting
    felt odd and inflexible to see '&#8210; {t("common:tsx.or")} &#8210;' between 2 phrases

--- src/components/recipe/suggestionToggle.tsx
+++ src/components/recipe/suggestionToggle.tsx
@@ -1,22 +1,39 @@
 "use client"
 
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useRecipe } from "./contextProvider";
 import RecipeEditor from "./editor/editor";
 import { RecipeSuggestions, suggestedRecipes } from "./suggested";
 import { useTranslation } from "react-i18next";
+import { LocaleContext } from "@/lib/i18nClient";
 
 export default function SuggestionToggle() {
   const { t } = useTranslation(["forms", "common"]);
-  
+
   const [visibilityType, setVisibilityType] = useState<"suggested" | "custom">("suggested")
   const { setRecipe } = useRecipe()
+
+  // For proper localization of the disjunction ("or") between the two options, should probably also work with languages modifying the input phrases themselves (e.g. adding affixes)?
+  const locale = useContext(LocaleContext);
+  const disjunctionFormatter = new Intl.ListFormat(locale, { style: 'long', type: 'disjunction' });
+  const sections = disjunctionFormatter.formatToParts([
+    t("forms:goal.select_recipe"),
+    t("forms:goal.create_recipe")
+  ]);
+
+  const firstItem = sections[0].value;
+  const disjunctionPhrase = sections[1].value;
+  const secondItem = sections[2].value;
+
+  if (sections.length !== 3 || sections[0].type !== 'element' || sections[1].type !== 'literal' || sections[2].type !== 'element') {
+    console.error("Unexpected format from Intl.ListFormat:", sections);
+  }
 
   return (
     <>
       <div className="radio-select-two margin-bottom-100" >
         <label id="recipe-type-suggested-label">
-          {t("forms:goal.select_recipe")}
+          {firstItem}
           <input
             className="margin-right-25"
             type="radio"
@@ -27,9 +44,9 @@ export default function SuggestionToggle() {
             onChange={() => { setVisibilityType("suggested"); setRecipe(null) }}
           />
         </label>
-        <span>&#8210; {t("common:tsx.or")} &#8210;</span>
+        <span>{`—${disjunctionPhrase}—`}</span>
         <label>
-          {t("forms:goal.create_recipe")}
+          {secondItem}
           <input
             className="margin-right-25"
             type="radio"
@@ -41,7 +58,7 @@ export default function SuggestionToggle() {
           />
         </label>
       </div>
-       {visibilityType === "suggested" ?
+      {visibilityType === "suggested" ?
         <div className="margin-top-100">
           <RecipeSuggestions ariaLabelledBy="recipe-type-suggested-label" suggestedRecipes={suggestedRecipes} />
         </div>
