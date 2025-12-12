1466217a6c6d06bfa544b3a33e6e73dcc87fa4ec
VenaStrom <vena.strom@sustainable-action.org>
2025-12-12 16:30:06 +0100

    add debug view to recipe context

--- src/components/recipe/contextProvider.tsx
+++ src/components/recipe/contextProvider.tsx
@@ -2,7 +2,7 @@
 
 import { emptyRecipe, Recipe } from "@/functions/recipe-parser/types";
 import type { DataSeriesValueFields } from "@/types";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 
 import { evaluateRecipe, cleanRecipe } from "@/functions/parseRecipe";
 
@@ -78,8 +78,68 @@ export function RecipeContextProvider({
     calculate().catch(e => { throw e; });
   }, [recipe]);
 
+  // Register debug key bind alt+shift+d
+  const [showDebug, setShowDebug] = useState(false);
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.altKey && event.shiftKey && event.key === "D") {
+        setShowDebug(prev => !prev);
+      }
+    }
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
     <RecipeContext.Provider value={{ recipe, setRecipe, warnings, error, resultingDataSeries, resultingUnit }}>
+      {showDebug &&
+        <div style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0,0,0,0.7)",
+          display: "flex",
+          flexFlow: "column nowrap",
+          justifyContent: "start",
+          alignItems: "start",
+          rowGap: "1rem",
+          padding: "1rem",
+          zIndex: 9999,
+        }}>
+          <pre style={{
+            color: "white",
+            overflow: "scroll",
+            width: "100%"
+          }}>
+            {JSON.stringify(recipe, null, 2)}
+          </pre>
+
+          <div className="flex gap-100">
+            <button
+              type="button"
+              onClick={() => setShowDebug(false)}
+            >
+              Close Debug
+            </button>
+
+            <button
+              type="button"
+              onClick={() => {
+                navigator.clipboard.writeText(JSON.stringify(recipe, null, 2))
+                  .catch((e) => {
+                    console.error(e);
+                  });
+              }}
+            >
+              Copy Recipe to Clipboard
+            </button>
+          </div>
+        </div>
+      }
       {children}
     </RecipeContext.Provider>
   );
