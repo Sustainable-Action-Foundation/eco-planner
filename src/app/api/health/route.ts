f4ee2448d6cacd132222a0379a7d1fa0c365e0be
VenaStrom <vena.strom@sustainable-action.org>
2025-12-18 14:30:06 +0100

    add commit hash to health route

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -1,6 +1,30 @@
 import { NextResponse } from "next/server";
+import fs from "node:fs";
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function GET() {
-  return new NextResponse(null, { status: 200, statusText: "OK" });
+  const body = { commitHash: "MISSING" };
+
+  try {
+    const commitHashes: unknown = JSON.parse(fs.readFileSync("src/lib/commitHash.json").toString());
+
+    if (
+      typeof commitHashes !== "object"
+      || commitHashes === null
+      || !("longHash" in commitHashes)
+      || typeof commitHashes["longHash"] !== "string"
+    ) {
+      throw new Error("Invalid commit hash file format");
+    }
+
+    body.commitHash = commitHashes.longHash;
+  }
+  catch (e) {
+    console.error("Failed to read commit hash file:", e);
+  }
+
+  return new NextResponse(
+    JSON.stringify(body),
+    { status: 200, statusText: "OK" }
+  );
 }
