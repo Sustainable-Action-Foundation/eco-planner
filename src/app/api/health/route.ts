9bc5ddd6f465e4198d0ce8df9d0b15098f2af0d5
VenaStrom <vena.strom@sustainable-action.org>
2025-12-19 13:13:05 +0100

    update health route

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -3,7 +3,14 @@ import fs from "node:fs";
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function GET() {
-  const body = { commitHash: "MISSING" };
+  const body = { shortHash: process.env.GIT_SHORT_HASH, longHash: process.env.GIT_LONG_HASH };
+
+  if (body.shortHash && body.longHash) {
+    return new NextResponse(
+      JSON.stringify(body),
+      { status: 200, statusText: "OK" }
+    );
+  }
 
   try {
     const commitHashes: unknown = JSON.parse(fs.readFileSync("src/lib/commitHash.json").toString());
@@ -11,13 +18,23 @@ export async function GET() {
     if (
       typeof commitHashes !== "object"
       || commitHashes === null
-      || !("longHash" in commitHashes)
-      || typeof commitHashes["longHash"] !== "string"
+
+      // Either long or short hash must be present
+      || (
+        !("shortHash" in commitHashes) && !("longHash" in commitHashes)
+      )
+      || ("shortHash" in commitHashes && typeof commitHashes.shortHash !== "string")
+      || ("longHash" in commitHashes && typeof commitHashes.longHash !== "string")
     ) {
       throw new Error("Invalid commit hash file format");
     }
 
-    body.commitHash = commitHashes.longHash;
+    if ("shortHash" in commitHashes && typeof commitHashes.shortHash === "string") {
+      body.shortHash = commitHashes.shortHash;
+    }
+    if ("longHash" in commitHashes && typeof commitHashes.longHash === "string") {
+      body.longHash = commitHashes.longHash;
+    }
   }
   catch (e) {
     console.error("Failed to read commit hash file:", e);
