370469dc009f706b417e2d41b05db15a48e39ad6
Axel <axel.schnurer@gmail.com>
2026-01-22 10:32:37 +0100

    Move historical data-stuff to its own page

--- src/app/goal/[goalId]/historical-data/page.tsx
+++ src/app/goal/[goalId]/historical-data/page.tsx
@@ -1,5 +1,76 @@
-export default function page() {
-    return (
-        <h1>:)</h1>
-    )
+import HistoricalData from "@/components/form/forms/historicalData";
+import getOneGoal from "@/fetchers/getOneGoal";
+import getOneRoadmap from "@/fetchers/getOneRoadmap";
+import getRoadmaps from "@/fetchers/getRoadmaps";
+import { buildMetadata } from "@/functions/buildMetadata";
+import serveTea from "@/lib/i18nServer";
+import { getSession } from "@/lib/session";
+import { cookies } from "next/headers";
+
+export async function generateMetadata(props: {
+  params: Promise<{ goalId: string }>,
+  searchParams: Promise<{
+    secondaryGoal?: string | string[] | undefined,
+    [key: string]: string | string[] | undefined
+  }>,
+}) {
+  const params = await props.params;
+
+  const [t, session, goal] = await Promise.all([
+    serveTea("metadata"),
+    getSession(await cookies()),
+    getOneGoal(params.goalId),
+  ]);
+
+  if (!session.user?.isLoggedIn) {
+    return buildMetadata({
+      title: t("metadata:login.title"),
+      description: t("metadata:login.title"),
+      og_url: `/goal/${params.goalId}`,
+      og_image_url: '/images/og_wind.png'
+    })
+  }
+
+  return buildMetadata({
+    title: goal?.name,
+    description: goal?.description,
+    og_url: `/goal/${params.goalId}`,
+    og_image_url: undefined, // TODO: Use graph api here once ready 
+  })
+}
+
+export default async function page(
+  props: {
+    params: Promise<{ goalId: string }>,
+    searchParams: Promise<{
+      secondaryGoal?: string | string[] | undefined,
+      [key: string]: string | string[] | undefined
+    }>,
+  }
+) {
+  const [params, searchParams] = await Promise.all([
+    props.params,
+    props.searchParams
+  ]);
+
+  const [t, session, { goal, roadmap }, secondaryGoal, unfilteredRoadmapOptions] = await Promise.all([ // TODO: Remove stuff i dont actually need
+    serveTea("pages"),
+    getSession(await cookies()),
+    getOneGoal(params.goalId).then(async goal => { return { goal, roadmap: (goal ? await getOneRoadmap(goal.roadmapId) : null) } }),
+    typeof searchParams.secondaryGoal == "string" ? getOneGoal(searchParams.secondaryGoal) : Promise.resolve(null),
+    getRoadmaps(),
+  ]);
+
+  if (!goal) {
+    return null;
+  }
+
+  return (
+    <div className="container-text margin-inline-auto">
+      <h1 className='margin-block-300 padding-bottom-100 margin-right-300' style={{ borderBottom: '1px solid var(--gray-90)' }}>
+        Lägg till historisk data {/* TODO: I18n */}
+      </h1>
+      <HistoricalData goal={goal} />
+    </div>
+  )
 }
