2ca1c62b6164bfae1e41bf04e01972db245ce54e
Noëlle <leon.loov@sustainable-action.org>
2026-01-23 16:34:31 +0100

    update health route to be more similar to info page

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -12,32 +12,40 @@ export async function GET() {
     );
   }
 
-  try {
-    const commitHashes: unknown = JSON.parse(fs.readFileSync("src/lib/commitHash.json").toString());
+  // If hash is not set in env, try to get it from file
+  if (!body.shortHash && !body.longHash) {
+    try {
+      if (fs.existsSync("src/lib/commitHash.json")) {
+        const parsedVersion: unknown = JSON.parse(fs.readFileSync("src/lib/commitHash.json", "utf-8"));
 
-    if (
-      typeof commitHashes !== "object"
-      || commitHashes === null
+        // Set hashes if they are properly formatted
+        if (parsedVersion instanceof Object && !(parsedVersion instanceof Array)) {
+          if ("shortHash" in parsedVersion && typeof parsedVersion.shortHash === "string") {
+            body.shortHash = parsedVersion.shortHash;
+          }
+          if ("longHash" in parsedVersion && typeof parsedVersion.longHash === "string") {
+            body.longHash = parsedVersion.longHash;
+          }
 
-      // Either long or short hash must be present
-      || (
-        !("shortHash" in commitHashes) && !("longHash" in commitHashes)
-      )
-      || ("shortHash" in commitHashes && typeof commitHashes.shortHash !== "string")
-      || ("longHash" in commitHashes && typeof commitHashes.longHash !== "string")
-    ) {
-      throw new Error("Invalid commit hash file format");
-    }
-
-    if ("shortHash" in commitHashes && typeof commitHashes.shortHash === "string") {
-      body.shortHash = commitHashes.shortHash;
-    }
-    if ("longHash" in commitHashes && typeof commitHashes.longHash === "string") {
-      body.longHash = commitHashes.longHash;
+          if (!body.shortHash && !body.longHash) {
+            throw new Error("Commit hash file does not contain valid hashes");
+          }
+        } else {
+          throw new Error("Invalid commit hash file format");
+        }
+      } else {
+        throw new Error("Commit hash file does not exist");
+      }
+    } catch (e) {
+      console.error("No commit hash in environment and failed to read commit hash file:", e);
     }
   }
-  catch (e) {
-    console.error("Failed to read commit hash file:", e);
+
+  if ("shortHash" in body && typeof body.shortHash === "string") {
+    body.shortHash = body.shortHash;
+  }
+  if ("longHash" in body && typeof body.longHash === "string") {
+    body.longHash = body.longHash;
   }
 
   return new NextResponse(
