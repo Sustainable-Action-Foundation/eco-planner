574fbcd266d3b711f77e23fa9cd4bc6befea084a
VenaStrom <vena.strom@sustainable-action.org>
2026-01-27 14:12:38 +0100

    repair weightedAverageDelta

--- src/functions/weightedAverageDelta.ts
+++ src/functions/weightedAverageDelta.ts
@@ -1,74 +1,85 @@
-import { DataSeries } from "@prisma/client";
+import { DataSeries, DateValues, isISOIshDate } from "@/types";
+import { dataSeriesToDateValues } from "./recipe/extractors";
+
+const YEARLY_INTEREST_DECAY = 0.1;
 
 /**
- * Returns a number representing how "interesting" a data series is. Bigger numbers are more "interesting".
+ * Returns a number in the range 0–1 representing how "interesting" a data series is.
  * 
- * The number is based on how large the changes are relative to the values (an increase from 1 => 2 is more interesting than 100 => 101)
+ * The number is based on how large the changes are relative to the max change in the series
  * and how close they are in time from now (a change next year is more interesting than a change in 10 years).
  */
-export default function dataSeriesInterest(dataSeries: DataSeries) {
-  if (!dataSeries) {
+export default function dataSeriesInterest(dataSeries: DataSeries): number {
+  const currentDate = new Date();
+
+  const dateValues = dataSeriesToDateValues(dataSeries);
+  const dates = Object.keys(dateValues.dateValues).sort();
+  if (!dates.every(isISOIshDate)) {
+    throw new Error("Data series contains non-date keys");
+  }
+  if (!dates.length) {
     return 0;
   }
 
-  const currentYear = new Date().getFullYear();
+  const derivedSeries = derive(dateValues.dateValues);
 
-  const keys = Years;
-  const years = keys.map((key) => parseInt(key.replace('val', '')));
+  let maxAbsDerivative = 0;
+  for (const value of Object.values(derivedSeries)) {
+    if (Number.isFinite(value)) {
+      const absValue = Math.abs(value);
+      if (absValue > maxAbsDerivative) {
+        maxAbsDerivative = absValue;
+      }
+    }
+  }
+  if (maxAbsDerivative === 0) {
+    return 0;
+  }
 
-  /**
-   * The max weight is twice the maximum number of years away from now in the data series.
-   * For example, if a data series goes from 2020 to 2050 and the current year is 2025, the max weight is 50.
-   * 
-   * The reason for this is that we want changes close to now to weigh more than ones in a long time or a long time ago,
-   * and we opted to give ones close in time up to twice the weight of those far away.
-   */
-  const maxWeight = Math.max(Math.abs(currentYear - Math.min(...years)), Math.abs(currentYear - Math.max(...years))) * 2;
-
-  let previousValue: number | undefined = undefined;
-  let previousYearWithValue: number | undefined = undefined;
-  let totalWeightedDelta = 0;
-  let totalValue = 0;
-
-  for (const key of keys) {
-    if (dataSeries[key] == null) {
-      continue;
-    } else if (previousValue == undefined || previousYearWithValue == undefined) {
-      previousValue = dataSeries[key];
-      previousYearWithValue = parseInt(key.replace('val', ''));
-      continue;
-    } else {
-      // Create array with all years from (exclusive) previousYearWithValue to the year of the current value (inclusive)
-      // This is done in order to handle years with undefined values, by assuming there's a linear change across the missing years. Works just as well with all values defined.
-      // We know previousYearWithValue is not null since we checked it earlier
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const relevantYears = Array.from({ length: parseInt(key.replace('val', '')) - previousYearWithValue }, (_, iter) => (previousYearWithValue! + 1) + iter);
-      // Total change since last noted value
-      const delta = dataSeries[key] - previousValue;
-      // Average change per year
-      const deltaPerYear = delta / relevantYears.length;
-      // Calculate weight for each year involved
-      const yearlyWeights = relevantYears.map((year) => maxWeight - Math.abs(currentYear - year));
-      // Sum the weights together
-      const subTotalWeight = yearlyWeights.reduce((partialWeight, a) => partialWeight + a, 0);
-      // Increase totalWeightedDelta by the average change per year multiplied by total weight for the range
-      // (= avg delta * avg weight * nr of years)
-      totalWeightedDelta += Math.abs(deltaPerYear * subTotalWeight);
-
-      // Get values for each year (assuming linear change if any years are missing values)
-      // We use absolute values to allow summing positive and negative values together
-      // We know dataSeries[key] is not null since we checked it earlier
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const yearlyValues = relevantYears.map((_, i) => Math.abs(dataSeries[key]! - deltaPerYear * i));
-      // increase toatlValue by sum of yearlyValues
-      totalValue += yearlyValues.reduce((partialValue, a) => partialValue + a, 0);
+  // Finds and weights the most "interesting" point in the series with respect to the max derivative and time decay
+  const interests: number[] = [];
+  for (const date of Object.keys(derivedSeries)) {
+    if (!isISOIshDate(date)) {
+      throw new Error("Data series contains non-date keys");
+    }
+    const value = derivedSeries[date];
+    if (Number.isFinite(value)) {
+      const year = new Date(date).getUTCFullYear();
+      const yearsOut = Math.abs(currentDate.getUTCFullYear() - year);
+      const magnitudeScore = Math.abs(value) / maxAbsDerivative;
+      const timeScore = Math.pow(1 - YEARLY_INTEREST_DECAY, yearsOut);
+      const interest = magnitudeScore * timeScore;
+      interests.push(interest);
     }
   }
 
-  let interest = totalWeightedDelta / totalValue;
-  // If interest is infinite or NaN it's because totalValue is 0, which is a very boring data series (the values 1 and -1 would give a totalValue of 2)
-  if (!Number.isFinite(interest)) {
-    interest = 0;
+  // Return average interest
+  const averageInterest = interests.reduce((sum, val) => sum + val, 0) / interests.length;
+  return averageInterest;
+}
+
+function derive(dataSeries: DateValues): DateValues {
+  const dates = Object.keys(dataSeries).sort();
+  const derived: DateValues = {};
+
+  for (let i = 1; i < dates.length; i++) {
+    const currentDate = dates[i];
+    const previousDate = dates[i - 1];
+
+    if (!isISOIshDate(currentDate) || !isISOIshDate(previousDate)) {
+      throw new Error("Data series contains non-date keys");
+    }
+
+    const currentValue = dataSeries[currentDate];
+    const previousValue = dataSeries[previousDate];
+
+    if (Number.isFinite(currentValue) && Number.isFinite(previousValue)) {
+      const dateDiff = (new Date(currentDate).getTime() - new Date(previousDate).getTime()) / (1000 * 60 * 60 * 24 * 365.24); // difference in days
+      derived[currentDate] = (currentValue - previousValue) / dateDiff;
+    } else {
+      // No-op
+    }
   }
-  return interest;
+
+  return derived;
 }
