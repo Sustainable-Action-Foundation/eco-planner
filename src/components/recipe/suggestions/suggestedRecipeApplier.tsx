91c892cad2a66d78487ba298df58ae8ace33f812
Noëlle <leon.loov@outlook.com>
2026-03-13 11:54:17 +0100

    Disable a network-based check running way too eagerly on load

--- src/components/recipe/suggestions/suggestedRecipeApplier.tsx
+++ src/components/recipe/suggestions/suggestedRecipeApplier.tsx
@@ -56,22 +56,23 @@ export function SuggestedRecipeApplier({
     fetchRoadmaps().catch(e => { throw e; });
   }, [t]);
 
-  // Validate suggested recipes
+  // Validate suggested recipe structures
   useEffect(() => {
-    async function validateAll() {
+    // async function validateAll() {
       for (const dbRecipe of suggestedRecipes) {
         const recipe = SmartRecipe.fromObject(dbRecipe.recipe);
         if (!isSmartRecipe(recipe)) {
           console.warn("Invalid recipe in suggestions", dbRecipe);
           return;
         }
-        const validity = await recipe.checkValidity();
-        if (!validity.good) {
-          console.warn("Invalid recipe in suggestions", dbRecipe, validity.error, validity.warnings);
-        }
+        // Disabled since this checks ALL suggested recipes EVERY TIME SOMEONE OPENS THE PAGE containing this component, not even just when they actually open the related modal
+        // const validity = await recipe.checkValidity();
+        // if (!validity.good) {
+        //   console.warn("Invalid recipe in suggestions", dbRecipe, validity.error, validity.warnings);
+        // }
       }
-    }
-    validateAll().catch(e => { throw e; });
+    // }
+    // validateAll().catch(e => { throw e; });
   }, [suggestedRecipes]);
 
   // On change set the context state to the selected recipe
