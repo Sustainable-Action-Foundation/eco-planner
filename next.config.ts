75cb9e7244f28fbef9dfe721d128fb3a58f2c09e
VenaStrom <vena.strom@sustainable-action.org>
2026-03-24 11:39:56 +0100

    remove webpack config from next config

--- next.config.ts
+++ next.config.ts
@@ -12,27 +12,6 @@ const nextConfig: NextConfig = {
     }
   } : {}),
   output: process.env.CI ? 'standalone' : undefined,
-  webpack: (
-    config,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-
-    // Ignore "src/scripts" folder in the build process
-    config.module.rules.push({
-      test: /src\/scripts/,
-      use: 'ignore-loader',
-    });
-
-    return config;
-  },
-  turbopack: {
-    rules: {
-      "/src/scripts/": {
-        loaders: ['ignore-loader'],
-      },
-    },
-  },
   experimental: {
     useCache: true,
   }
