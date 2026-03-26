bc32627f13f77e62b4847fe878d6144f753c35a6
VenaStrom <vena.strom@sustainable-action.org>
2026-03-26 10:50:55 +0100

    WIP use correct tseslint for non app code

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,47 +1,42 @@
 import path from "node:path";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { Config, defineConfig, globalIgnores } from "eslint/config";
 import nextTS from "eslint-config-next/typescript";
 import nextVitals from "eslint-config-next/core-web-vitals";
+import tseslint from "typescript-eslint";
+
+const nonAppTsBaseConfig = tseslint.configs.recommendedTypeChecked;
+
+const tsCommonRules: Config["rules"] = {
+  "prefer-const": "warn",
+  "@typescript-eslint/no-unused-vars": [
+    "warn",
+    {
+      "args": "all",
+      "argsIgnorePattern": "^_",
+      "caughtErrors": "all",
+      "caughtErrorsIgnorePattern": "^_",
+      "destructuredArrayIgnorePattern": "^_",
+      "varsIgnorePattern": "^_",
+      "ignoreRestSiblings": true
+    }
+  ],
+  "@typescript-eslint/no-unsafe-argument": "warn",
+  "@typescript-eslint/no-unsafe-assignment": "warn",
+  "@typescript-eslint/no-unsafe-call": "warn",
+  "@typescript-eslint/no-unsafe-return": "error",
+  "@typescript-eslint/no-unsafe-member-access": "warn",
+  "@typescript-eslint/no-unsafe-enum-comparison": "warn",
+  "@typescript-eslint/no-non-null-assertion": "warn",
+  "@typescript-eslint/no-confusing-non-null-assertion": "error",
+  "@typescript-eslint/require-await": "warn",
+  "@typescript-eslint/no-misused-promises": "warn",
+  "@typescript-eslint/no-floating-promises": "warn",
+  "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+};
 
 export default defineConfig([
-  ...nextTS, // This is a next flavor of tseslint and it is being used on non next files... (scripts, test) but that is probably fine enough
-  { // TS rules for all files
-    files: ["**/*.{ts,tsx}"],
-    rules: {
-      "prefer-const": "warn",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          "args": "all",
-          "argsIgnorePattern": "^_",
-          "caughtErrors": "all",
-          "caughtErrorsIgnorePattern": "^_",
-          "destructuredArrayIgnorePattern": "^_",
-          "varsIgnorePattern": "^_",
-          "ignoreRestSiblings": true
-        }
-      ],
-      "@typescript-eslint/no-unsafe-argument": "warn",
-      "@typescript-eslint/no-unsafe-assignment": "warn",
-      "@typescript-eslint/no-unsafe-call": "warn",
-      "@typescript-eslint/no-unsafe-return": "error",
-      "@typescript-eslint/no-unsafe-member-access": "warn",
-      "@typescript-eslint/no-unsafe-enum-comparison": "warn",
-      "@typescript-eslint/no-non-null-assertion": "warn",
-      "@typescript-eslint/no-confusing-non-null-assertion": "error",
-      "@typescript-eslint/require-await": "warn",
-      "@typescript-eslint/no-misused-promises": "warn",
-      "@typescript-eslint/no-floating-promises": "warn",
-      "@typescript-eslint/no-unnecessary-type-assertion": "warn",
-    },
-    languageOptions: {
-      parserOptions: {
-        project: "./tsconfig.json",
-        tsconfigRootDir: process.cwd(),
-      },
-    },
-  },
   { // App linting
+    ...nextTS,
     name: "App src/",
     files: ["src/**/*.{ts,tsx}"],
     extends: [
@@ -51,6 +46,7 @@ export default defineConfig([
       "react-hooks/set-state-in-effect": "warn",
       "react-hooks/set-state-in-render": "warn",
       "react-hooks/immutability": "warn", // This should probably be a warning but the current recipe pipeline is dependant on it :sweat_smile:
+      ...tsCommonRules,
     },
     languageOptions: {
       parserOptions: {
@@ -60,8 +56,12 @@ export default defineConfig([
     },
   },
   { // Test linting
+    ...nonAppTsBaseConfig,
     name: "Tests tests/",
     files: ["tests/**/*.{ts,tsx}"],
+    rules: {
+      ...tsCommonRules,
+    },
     languageOptions: {
       parserOptions: {
         project: "./tsconfig.json",
@@ -70,8 +70,12 @@ export default defineConfig([
     },
   },
   { // Script linting
+    ...nonAppTsBaseConfig,
     name: "scripts scripts/",
     files: ["scripts/**/*.{ts,tsx}"],
+    rules: {
+      ...tsCommonRules,
+    },
     languageOptions: {
       parserOptions: {
         project: "./tsconfig.json",
