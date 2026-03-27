2c0b7632f67f5767457aea161772c553b7f21591
VenaStrom <vena.strom@sustainable-action.org>
2026-03-27 13:34:32 +0100

    ignore strings and bools on prefer-nullish-coalescing

--- eslint.config.ts
+++ eslint.config.ts
@@ -37,12 +37,13 @@ const tsCommonRules: Config["rules"] = {
   "@typescript-eslint/consistent-type-exports": "warn",
   "@typescript-eslint/consistent-type-definitions": ["warn", "type"],
   "@typescript-eslint/ban-ts-comment": "error",
-  "@typescript-eslint/prefer-nullish-coalescing": "warn",
+  "@typescript-eslint/prefer-nullish-coalescing": ["warn", { ignorePrimitives: { string: true, boolean: true, }, },],
   "@typescript-eslint/prefer-optional-chain": "warn",
   "@typescript-eslint/restrict-template-expressions": "warn",
   "@typescript-eslint/no-base-to-string": "warn",
   "@typescript-eslint/use-unknown-in-catch-callback-variable": "error",
   "@/no-useless-assignment": "warn",
+  // "eqeqeq": ["error", "smart"], // TODO - use
 };
 
 export default defineConfig([
