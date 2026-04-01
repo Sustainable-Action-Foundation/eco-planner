9be8edd7859aa51d63a5339aaec5d96cdb071914
pysor2 <erikssonrazze@gmail.com>
2026-04-01 11:00:40 +0200

    separate the full page and non full page images in the screenshot viewer

--- tests/screenshotViewer/screenshotViewer.ts
+++ tests/screenshotViewer/screenshotViewer.ts
@@ -1,39 +1,48 @@
 import fs from "node:fs"
-import path, { dirname } from "node:path"
-
 
 const viewPage = fs.readFileSync("tests/screenshotViewer/screenshotViewer.html").toString();
 const cardComponent = fs.readFileSync("tests/screenshotViewer/components/card.html").toString();
 const imgComponent = fs.readFileSync("tests/screenshotViewer/components/img.html").toString();
 const screenshotDirs = fs.readdirSync("tests/screenshots");
 
-
-
 function createViewPage() {
   let page = "";
 
-  let head = "";
-  let card = "";
   let cards = [""];
 
-  let img = "";
-  let imgCard = "";
-  let imgs = [""];
 
   screenshotDirs.forEach(catName => {
+    let card = "";
+    let fullPageCard = "";
 
-    head = cardComponent.replace("{{HEADER}}", catName);
+    let img = "";
+    let imgCard = "";
+    let imgCards = [""];
+    let fullPageImgs = [""];
 
     const thisDir = fs.readdirSync("tests/screenshots/" + catName);
-    thisDir.forEach(picture => {
-      img = imgComponent.replace("{{SRC}}", "../screenshots/" + catName + "/" + picture);
-      imgCard = img.replace("{{ALT}}", picture);
-      imgs.push(imgCard);
-    });
-    card = head.replace("{{IMGS}}", imgs.toString())
+    thisDir.forEach(capture => {
+      if (capture.endsWith("fullPage.jpeg")) {
+        card = cardComponent.replace("{{HEADER}}", catName + "FullPage");
 
-    // card = card.replaceAll(",", "")
-    cards.push(card)
+        img = imgComponent.replace("{{PROJECT}}", capture)
+        img = img.replace("{{SRC}}", "../screenshots/" + catName + "/" + capture);
+        imgCard = img.replace("{{ALT}}", capture);
+        fullPageImgs.push(imgCard);
+      } else {
+        fullPageCard = cardComponent.replace("{{HEADER}}", catName);
+
+        img = imgComponent.replace("{{PROJECT}}", capture)
+        img = img.replace("{{SRC}}", "../screenshots/" + catName + "/" + capture);
+        imgCard = img.replace("{{ALT}}", capture);
+        imgCards.push(imgCard);
+      }
+    });
+    card = card.replace("{{IMGS}}", imgCards.toString());
+    fullPageCard = fullPageCard.replace("{{IMGS}}", fullPageImgs.toString());
+
+    cards.push(card);
+    cards.push(fullPageCard);
   });
   page = viewPage.replace("{{BODY}}", cards.toString())
   page = page.replaceAll(",", "")
@@ -44,4 +53,4 @@ createViewPage();
 
 
 
-fs.globSync("tests/screenshots/**/*.jpeg")
+// fs.globSync("tests/screenshots/**/*.jpeg")
