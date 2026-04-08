973f8c7f170c41f123a633b855ccfc71d5e33a9b
VenaStrom <vena.strom@sustainable-action.org>
2026-04-08 13:04:01 +0200

    add explicit `node:` to imports

--- tests/login.setup.ts
+++ tests/login.setup.ts
@@ -1,7 +1,7 @@
 import { test as setup, expect } from 'playwright/test';
 import type { Page } from 'playwright/test';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
