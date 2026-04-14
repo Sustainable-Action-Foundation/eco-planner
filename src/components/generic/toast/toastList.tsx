0f2d9acaa185a690324acb66decb0dc7d90353e0
Linus <elistewesterlinus@gmail.com>
2026-04-14 11:23:37 +0200

    Move all inline styling for toastList to its class

--- src/components/generic/toast/toastList.tsx
+++ src/components/generic/toast/toastList.tsx
@@ -6,12 +6,14 @@ import styles from './toast.module.css'
 export default function ToastList() {
   const { messages } = useToastContext();
   return (
-    <output className={styles.toastCard + " position-fixed flex flex-direction-column-reverse gap-50"} style={{ top: "24px", right: "16px", zIndex: "calc(infinity * 1)" }} aria-live="polite">
-      {messages.map((message) => (
-        <CreateToast key={message.id} id={message.id} type={message.type} hasTimeout={message.hasTimeout}>
-          {message.text}
-        </CreateToast>
-      ))}
-    </output>
+    <aside>
+      <output className={styles.toastCard + " position-fixed flex flex-direction-column-reverse gap-50"} aria-live="polite">
+        {messages.map((message) => (
+          <CreateToast key={message.id} id={message.id} type={message.type} hasTimeout={message.hasTimeout}>
+            {message.text}
+          </CreateToast>
+        ))}
+      </output>
+    </aside>
   )
 }
