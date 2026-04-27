adbe3814844ef33a844fc7c256d4eff212700a60
VenaStrom <vena.strom@sustainable-action.org>
2026-04-27 13:54:28 +0200

    simplify git hash collection and other env

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,59 @@
+import { GlobalEnv } from "@/types";
 import type { NextConfig } from "next";
+import { execSync } from "node:child_process";
+import packageJSON from "package.json" with { type: "json" };
+
+function getCommitHash(): { sha: string, dirty: boolean } {
+  const hashInfo: { sha: string; dirty: boolean; } = {
+    sha: process.env.COMMIT_SHA,
+    dirty: false,
+  };
+
+  if (hashInfo.sha) {
+    console.info(`Commit hash found in environment variable: (${hashInfo.sha})`);
+    return hashInfo;
+  }
+
+  // Try getting commit hashes via git cli
+  try {
+    const headCommit = execSync("git rev-parse HEAD")?.toString().trim();
+    const cleanStatus = !execSync("git status --porcelain")?.toString().trim();
+
+    if (!cleanStatus) {
+      hashInfo.sha = `dirty-${headCommit}`;
+      hashInfo.dirty = true;
+    }
+    else {
+      hashInfo.sha = headCommit;
+    }
+  }
+  catch (err: unknown) {
+    console.warn("Failed to get commit hashes", { err })
+  }
+
+  if (!hashInfo.sha) {
+    console.error("No commit hash found.");
+    throw new Error("No commit hash found. Ensure that the build environment has access to git information, or set the COMMIT_SHA environment variable manually.");
+  }
+
+  return hashInfo;
+}
+
+const { sha, dirty: dirtyTree } = getCommitHash();
+
+const buildTimeEnv: GlobalEnv = {
+  APP_VERSION: process.env.npm_package_version ?? "unknown",
+  COMMIT_SHA: sha,
+  COMMIT_URL: dirtyTree ? undefined : new URL(`commit/${sha}`, packageJSON.homepage).toString(),
+  REMOTE_REPO_URL: packageJSON.homepage,
+} as const;
 
 const nextConfig: NextConfig = {
   devIndicators: {
     position: 'bottom-right'
   },
-  ...(process.env.NODE_ENV == "production" ? {
+  env: buildTimeEnv,
+  ...(process.env.NODE_ENV === "production" ? {
     compiler: {
       removeConsole: {
         exclude: ['info', 'error', 'warn']
@@ -14,7 +63,7 @@ const nextConfig: NextConfig = {
   output: process.env.CI ? 'standalone' : undefined,
   experimental: {
     useCache: true,
-  }
+  },
 }
 
 export default nextConfig
