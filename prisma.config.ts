e88f0d0ed45776652481acb9f15ac5ee125e2466
VenaStrom <vena.strom@sustainable-action.org>
2026-04-29 14:01:35 +0200

    remove path from prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,11 @@
 import "dotenv/config";
-import path from "node:path";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: path.join("prisma", "schema.prisma"),
+  schema: "prisma/schema.prisma",
 
   migrations: {
-    path: path.join("prisma", "migrations"),
+    path: "prisma/migrations",
     seed: "tsx scripts/prisma/seed.ts",
   },
   datasource: {
