5d8477b9f6fa4fa72ea786bb0d91ae47a2548f63
VenaStrom <vena.strom@sustainable-action.org>
2026-05-06 19:43:34 +0200

    log errors in generateLeapList

--- scripts/generateLeapList.ts
+++ scripts/generateLeapList.ts
@@ -26,13 +26,13 @@ async function generateLeapList() {
       },
     });
   }
-  catch {
-    console.log('Failed to fetch roadmaps for LEAP list generation.');
+  catch (err: unknown) {
+    console.error('Failed to fetch roadmaps for LEAP list generation.', { err });
     return;
   }
 
   if (rawData.length === 0) {
-    console.log("No public, national roadmaps found; LEAP list not touched.");
+    console.info("No public, national roadmaps found; LEAP list not touched.");
     return;
   }
 
@@ -53,9 +53,10 @@ async function generateLeapList() {
   // Write to file
   try {
     fs.writeFileSync('src/lib/LEAPList.json', JSON.stringify(uniqueLeapList));
-    console.log('LEAP list updated');
-  } catch {
-    console.log('Failed to update LEAP list');
+    console.info('LEAP list updated');
+  } 
+  catch (err: unknown) {
+    console.warn('Failed to write LEAP list file', { err });
   }
 }
 
