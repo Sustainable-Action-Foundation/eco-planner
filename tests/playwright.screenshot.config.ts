45737c50818f8e950ff52f90ff72cdcb81336506
Noëlle <noelle.loov@sustainable-action.org>
2026-05-12 18:00:42 +0200

    update playwright configs to use `testMatch` rather than `testIgnore`

--- tests/playwright.screenshot.config.ts
+++ tests/playwright.screenshot.config.ts
@@ -8,14 +8,12 @@ export const webserverURL = process.env.BASE_URL || "http://localhost:8081";
 
 const CI = boolEnv("CI", false);
 
-const testMatch = "screenshots/screenshot-tests.spec.ts";
-
 /**
   To run screenshot tests locally you must run: yarn test:screenshot
 */
 export default defineConfig({
   testDir: "./",
-  testIgnore: ["./**/", "!./screenshots/", "!./lib/"],
+  testMatch: "screenshots/**/*.spec.ts",
 
   workers: "80%",
 
@@ -93,19 +91,16 @@ export default defineConfig({
     {
       name: "chromium 1080p",
       use: { ...devices["Desktop Chrome"], viewport: { width: 1920, height: 1080 }, channel: "chromium" },
-      testMatch,
       dependencies: ["setup"],
     },
     {
       name: "firefox 1080p",
       use: { ...devices["Desktop Firefox"], viewport: { width: 1920, height: 1080 } },
-      testMatch,
       dependencies: ["setup"],
     },
     {
       name: "webkit 1080p",
       use: { ...devices["Desktop Safari"], viewport: { width: 1920, height: 1080 } },
-      testMatch,
       dependencies: ["setup"],
     },
     // Formats for screenshot tests
@@ -113,209 +108,176 @@ export default defineConfig({
     {
       name: "chromium 720p",
       use: { ...devices["Desktop Chrome"], viewport: { width: 1280, height: 720 }, channel: "chromium" },
-      testMatch,
       dependencies: ["setup"],
     },
     {
       name: "firefox 720p",
       use: { ...devices["Desktop Firefox"], viewport: { width: 1280, height: 720 } },
-      testMatch,
       dependencies: ["setup"],
     },
     {
       name: "webkit 720p",
       use: { ...devices["Desktop Safari"], viewport: { width: 1280, height: 720 } },
-      testMatch,
       dependencies: ["setup"],
     },
     // 1080p-vert
     {
       name: "chromium 1080p-vert",
       use: { ...devices["Desktop Chrome"], viewport: { width: 1080, height: 1920 }, channel: "chromium" },
-      testMatch,
       dependencies: ["setup"],
     },
     {
       name: "firefox 1080p-vert",
       use: { ...devices["Desktop Firefox"], viewport: { width: 1080, height: 1920 } },
-      testMatch,
       dependencies: ["setup"],
     },
     {
       name: "webkit 1080p-vert",
       use: { ...devices["Desktop Safari"], viewport: { width: 1080, height: 1920 } },
-      testMatch,
       dependencies: ["setup"],
     },
     // // 1440p
     // {
     //   name: "chromium 1440p",
     //   use: { ...devices["Desktop Chrome"], viewport: { width: 2560, height: 1440 }, channel: "chromium" },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "firefox 1440p",
     //   use: { ...devices["Desktop Firefox"], viewport: { width: 2560, height: 1440 } },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "webkit 1440p",
     //   use: { ...devices["Desktop Safari"], viewport: { width: 2560, height: 1440 } },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // 4k
     {
       name: "chromium 4k",
       use: { ...devices["Desktop Chrome"], viewport: { width: 3840, height: 2160 }, channel: "chromium" },
-      testMatch,
       dependencies: ["setup"],
     },
     {
       name: "firefox 4k",
       use: { ...devices["Desktop Firefox"], viewport: { width: 3840, height: 2160 } },
-      testMatch,
       dependencies: ["setup"],
     },
     {
       name: "webkit 4k",
       use: { ...devices["Desktop Safari"], viewport: { width: 3840, height: 2160 } },
-      testMatch,
       dependencies: ["setup"],
     },
     // // older office (4:3)
     // {
     //   name: "chromium office",
     //   use: { ...devices["Desktop Chrome"], viewport: { width: 1440, height: 1080 }, channel: "chromium" },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "firefox office",
     //   use: { ...devices["Desktop Firefox"], viewport: { width: 1440, height: 1080 } },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "webkit office",
     //   use: { ...devices["Desktop Safari"], viewport: { width: 1440, height: 1080 } },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // // Noëlle (2880 x 1920 at 225% zoom)
     // {
     //   name: "chromium Noëlle",
     //   use: { ...devices["Desktop Chrome"], deviceScaleFactor: 2.25, viewport: { width: 1280, height: 853 }, channel: "chromium" },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "firefox Noëlle",
     //   use: { ...devices["Desktop Firefox"], deviceScaleFactor: 2.25, viewport: { width: 1280, height: 853 } },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "webkit Noëlle",
     //   use: { ...devices["Desktop Safari"], deviceScaleFactor: 2.25, viewport: { width: 1280, height: 853 } },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // // Desktop HiDPI
     // {
     //   name: "chrome desktop HiDPI",
     //   use: { ...devices["Desktop Chrome HiDPI"], channel: "chromium" },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "firefox desktop HiDPI",
     //   use: { ...devices["Desktop Firefox HiDPI"] },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "webkit desktop HiDPI",
     //   use: { ...devices["Desktop Safari"] },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // iPhone 15
     // {
     //   name: "chromium iPhone 15",
     //   use: { ...devices["iPhone 15"], defaultBrowserType: "chromium", channel: "chromium" },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "firefox iPhone 15",
-    //   use: { ...devices["iPhone 15"], isMobile: false, defaultBrowserType: "firefox" }, // Firefox does not support isMobile: true ¯\_(ツ)_/¯ 
-    //   testMatch,
+    //   use: { ...devices["iPhone 15"], isMobile: false, defaultBrowserType: "firefox" }, // Firefox does not support isMobile: true ¯\_(ツ)_/¯
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "webkit iPhone 15",
     //   use: { ...devices["iPhone 15"], defaultBrowserType: "webkit" },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // // iPad (gen 11) landscape
     // {
     //   name: "chromium iPad (gen 11) landscape",
     //   use: { ...devices["iPad (gen 11) landscape"], defaultBrowserType: "chromium", channel: "chromium" },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "firefox iPad (gen 11) landscape",
-    //   use: { ...devices["iPad (gen 11) landscape"], isMobile: false, defaultBrowserType: "firefox" }, // Firefox does not support isMobile: true ¯\_(ツ)_/¯ 
-    //   testMatch,
+    //   use: { ...devices["iPad (gen 11) landscape"], isMobile: false, defaultBrowserType: "firefox" }, // Firefox does not support isMobile: true ¯\_(ツ)_/¯
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "webkit iPad (gen 11) landscape",
     //   use: { ...devices["iPad (gen 11) landscape"], defaultBrowserType: "webkit" },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // Galaxy S9+
     {
       name: "chromium Galaxy S9+",
       use: { ...devices["Galaxy S9+"], defaultBrowserType: "chromium", channel: "chromium" },
-      testMatch,
       dependencies: ["setup"],
     },
     {
       name: "firefox Galaxy S9+",
-      use: { ...devices["Galaxy S9+"], isMobile: false, defaultBrowserType: "firefox" }, // Firefox does not support isMobile: true ¯\_(ツ)_/¯ 
-      testMatch,
+      use: { ...devices["Galaxy S9+"], isMobile: false, defaultBrowserType: "firefox" }, // Firefox does not support isMobile: true ¯\_(ツ)_/¯
       dependencies: ["setup"],
     },
     {
       name: "webkit Galaxy S9+",
       use: { ...devices["Galaxy S9+"], defaultBrowserType: "webkit" },
-      testMatch,
       dependencies: ["setup"],
     },
     // // Pixel 7
     // {
     //   name: "chromium Pixel 7",
     //   use: { ...devices["Pixel 7"], defaultBrowserType: "chromium", channel: "chromium" },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "firefox Pixel 7",
-    //   use: { ...devices["Pixel 7"], isMobile: false, defaultBrowserType: "firefox" }, // Firefox does not support isMobile: true ¯\_(ツ)_/¯ 
-    //   testMatch,
+    //   use: { ...devices["Pixel 7"], isMobile: false, defaultBrowserType: "firefox" }, // Firefox does not support isMobile: true ¯\_(ツ)_/¯
     //   dependencies: ["setup"],
     // },
     // {
     //   name: "webkit Pixel 7",
     //   use: { ...devices["Pixel 7"], defaultBrowserType: "webkit" },
-    //   testMatch,
     //   dependencies: ["setup"],
     // },
 
